d9991c6ad37bf790d3bb5d7331c4eeb5acf79bf5
djewsbury <djewsbury@xlgames.com>
2015-03-17 18:35:35 +0900

    Looking for Visual Studio Designer linking problem

---
+++ Tools/GUILayer/AssemblyInfo.cpp
@@ -0,0 +1,36 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+//
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly:AssemblyTitleAttribute(L"XLEGUILayer")];
+[assembly:AssemblyDescriptionAttribute(L"Used as a layer between high level CLR GUI controls and native C++ XLE code")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"XLGames")];
+[assembly:AssemblyProductAttribute(L"XLEGUILayer")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) XLGames 2015")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the value or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly:AssemblyVersionAttribute("0.03.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
