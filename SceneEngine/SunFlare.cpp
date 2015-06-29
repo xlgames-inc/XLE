fbd745cd413d4f55e8ed56060aa5d034e79fca5f
djewsbury <djewsbury@xlgames.com>
2015-06-29 18:18:29 +0900

    Working on getting the occluding sun flare going - first pass; still working on optimisations

--- SceneEngine/SunFlare.cpp
+++ SceneEngine/SunFlare.cpp
@@ -7,12 +7,20 @@
 #include "SunFlare.h"
 #include "LightingParserContext.h"
 #include "SceneEngineUtils.h"
+#include "SceneParser.h"
+#include "LightDesc.h"
 #include "../RenderCore/Metal/InputLayout.h"
 #include "../RenderCore/Metal/Shader.h"
 #include "../RenderCore/Metal/DeviceContext.h"
+#include "../RenderCore/Metal/ShaderResource.h"
+#include "../BufferUploads/IBufferUploads.h"
+#include "../BufferUploads/ResourceLocator.h"
 #include "../RenderCore/Techniques/Techniques.h"
 #include "../RenderCore/Techniques/ResourceBox.h"
 #include "../RenderCore/Techniques/CommonResources.h"
+#include "../Math/Transformations.h"
+#include "../Utility/StringFormat.h"
+#include "../ConsoleRig/Console.h"
 #include "../Assets/Assets.h"
 #include "../Core/Exceptions.h"
 
@@ -23,14 +31,34 @@ namespace SceneEngine
     class SunFlareRes
     {
     public:
-        class Desc {};
+        class Desc 
+        {
+        public:
+            bool _singlePass;
+            Desc(bool singlePass)
+            {
+                XlZeroMemory(*this);
+                _singlePass = singlePass;
+            }
+        };
 
-        const RenderCore::Metal::ShaderProgram* _shader;
-        RenderCore::Metal::BoundUniforms _uniforms;
+        const RenderCore::Metal::ShaderProgram* _toRadialShader;
+        RenderCore::Metal::BoundUniforms _toRadialUniforms;
+
+        const RenderCore::Metal::ShaderProgram* _blurShader;
+        RenderCore::Metal::BoundUniforms _blurUniforms;
+        
+        const RenderCore::Metal::ShaderProgram* _commitShader;
+        RenderCore::Metal::BoundUniforms _commitUniforms;
+
+        const RenderCore::Metal::ShaderProgram* _directBlurShader;
+        RenderCore::Metal::BoundUniforms _directBlurUniforms;
 
         SunFlareRes(const Desc& desc);
 
-        const std::shared_ptr<::Assets::DependencyValidation>& GetDependencyValidation() const   { return _validationCallback; }
+        const std::shared_ptr<::Assets::DependencyValidation>& GetDependencyValidation() const
+            { return _validationCallback; }
+
     private:
         std::shared_ptr<Assets::DependencyValidation>  _validationCallback;
     };
@@ -38,17 +66,58 @@ namespace SceneEngine
     SunFlareRes::SunFlareRes(const Desc& desc)
     {
         using namespace RenderCore;
-        _shader = &::Assets::GetAssetDep<Metal::ShaderProgram>(
+
+        ////////////////////////////////////////////////////////////////////////
+
+        _commitShader = &::Assets::GetAssetDep<Metal::ShaderProgram>(
             "game/xleres/effects/occludingsunflare.sh:vs_sunflare:vs_*",
             "game/xleres/effects/occludingsunflare.sh:ps_sunflare:ps_*");
 
-        _uniforms = RenderCore::Metal::BoundUniforms(*_shader);
-        Techniques::TechniqueContext::BindGlobalUniforms(_uniforms);
-        _uniforms.BindConstantBuffer(Hash64("Settings"), 0, 1);
-        _uniforms.BindShaderResource(Hash64("DepthTexture"), 0, 1);
+        _commitUniforms = Metal::BoundUniforms(*_commitShader);
+        Techniques::TechniqueContext::BindGlobalUniforms(_commitUniforms);
+        _commitUniforms.BindConstantBuffers(1, {"Settings"});
+        _commitUniforms.BindShaderResources(1, {"InputTexture"});
+
+        ////////////////////////////////////////////////////////////////////////
+
+        _blurShader = &::Assets::GetAssetDep<Metal::ShaderProgram>(
+            "game/xleres/basic2d.vsh:fullscreen:vs_*",
+            "game/xleres/effects/occludingsunflare.sh:ps_blur:ps_*",
+            (StringMeld<64>() << "SINGLE_PASS=" << int(desc._singlePass)).get());
+
+        _blurUniforms = Metal::BoundUniforms(*_blurShader);
+        Techniques::TechniqueContext::BindGlobalUniforms(_blurUniforms);
+        _blurUniforms.BindConstantBuffers(1, {"Settings"});
+        _blurUniforms.BindShaderResources(1, {"InputTexture"});
+
+        ////////////////////////////////////////////////////////////////////////
+
+        _toRadialShader = &::Assets::GetAssetDep<Metal::ShaderProgram>(
+            "game/xleres/effects/occludingsunflare.sh:vs_sunflare:vs_*",
+            "game/xleres/effects/occludingsunflare.sh:ps_toradial:ps_*");
+
+        _toRadialUniforms = Metal::BoundUniforms(*_toRadialShader);
+        Techniques::TechniqueContext::BindGlobalUniforms(_toRadialUniforms);
+        _toRadialUniforms.BindConstantBuffers(1, {"Settings"});
+        _toRadialUniforms.BindShaderResources(1, {"InputTexture"});
+
+        ////////////////////////////////////////////////////////////////////////
+
+        _directBlurShader = &::Assets::GetAssetDep<Metal::ShaderProgram>(
+            "game/xleres/effects/occludingsunflare.sh:vs_sunflare:vs_*",
+            "game/xleres/effects/occludingsunflare.sh:ps_sunflare_directblur:ps_*");
+
+        _directBlurUniforms = Metal::BoundUniforms(*_directBlurShader);
+        Techniques::TechniqueContext::BindGlobalUniforms(_directBlurUniforms);
+        _directBlurUniforms.BindConstantBuffers(1, {"Settings"});
+        _directBlurUniforms.BindShaderResources(1, {"InputTexture"});
+
+        ////////////////////////////////////////////////////////////////////////
 
         _validationCallback = std::make_shared<::Assets::DependencyValidation>();
-        ::Assets::RegisterAssetDependency(_validationCallback, _shader->GetDependencyValidation());
+        ::Assets::RegisterAssetDependency(_validationCallback, _commitShader->GetDependencyValidation());
+        ::Assets::RegisterAssetDependency(_validationCallback, _blurShader->GetDependencyValidation());
+        ::Assets::RegisterAssetDependency(_validationCallback, _toRadialShader->GetDependencyValidation());
     }
 
     void SunFlare_Execute(
@@ -56,49 +125,149 @@ namespace SceneEngine
         LightingParserContext& parserContext,
         RenderCore::Metal::ShaderResourceView& depthsSRV)
     {
+        if (!parserContext.GetSceneParser() || !parserContext.GetSceneParser()->GetLightCount())
+            return;
+
+        using namespace RenderCore;
         SavedTargets savedTargets(context);
+        Metal::ViewportDesc savedViewport(*context);
         
         TRY 
         {
-            using namespace RenderCore;
-            context->GetUnderlying()->OMSetRenderTargets(1, savedTargets.GetRenderTargets(), nullptr);
+            const auto& sunDesc = parserContext.GetSceneParser()->GetLightDesc(0);
+            const auto cameraPos = ExtractTranslation(parserContext.GetProjectionDesc()._cameraToWorld);
+            const auto transConstants = BuildGlobalTransformConstants(parserContext.GetProjectionDesc());
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+            const auto worldToView = InvertOrthonormalTransform(parserContext.GetProjectionDesc()._cameraToWorld);
+            Float4 sunPos = 
+                parserContext.GetProjectionDesc()._worldToProjection 
+                * Expand(Float3(cameraPos + 10000.f * sunDesc._negativeLightDirection), 1.f);
+            float aspect = savedViewport.Height / savedViewport.Width;
+            float sunFlareDist = Tweakable("SunFlareDist", .5f);
 
             struct Settings
             {
-                Float4x4 _projectionmatrix;
-                Float3 _viewSpaceSunPosition;
-                unsigned _dummy;
-            } 
+                Float2 _projSpaceSunPosition;
+                Float2 _aspectCompenstation;
+            }
             settings = 
             {
-                parserContext.GetProjectionDesc()._cameraToProjection,
-                Float3(0.f, 0.f, 100.f), 0
+                Float2(sunPos[0] / sunPos[3], sunPos[1] / sunPos[3]),
+                Float2(2.f * sunFlareDist * aspect, 2.f * sunFlareDist)
             };
-            Metal::ConstantBufferPacket constants[] = { MakeSharedPkt(settings) };
-            const Metal::ShaderResourceView* srvs[] = { &depthsSRV };
 
-            const auto& res = Techniques::FindCachedBoxDep2<SunFlareRes>();
-            res._uniforms.Apply(
-                *context, 
-                parserContext.GetGlobalUniformsStream(),
-                Metal::UniformsStream(constants, srvs));
+            auto settingsPkt = MakeSharedPkt(settings);
 
-            context->Bind(Techniques::CommonResources()._blendStraightAlpha);
-            context->Bind(*res._shader);
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
             context->Bind(Metal::Topology::TriangleStrip);
-            context->Unbind<RenderCore::Metal::VertexBuffer>();
-            context->Unbind<RenderCore::Metal::BoundInputLayout>();
-            context->Draw(4);
+            context->Unbind<Metal::VertexBuffer>();
+            context->Unbind<Metal::BoundInputLayout>();
 
-            context->UnbindPS<RenderCore::Metal::ShaderResourceView>(3, 1);
+            const bool doDirectBlur = Tweakable("SunFlareDirectBlur", false);
+            const bool singlePass = Tweakable("SunFlareSinglePass", false);
+
+            const auto& res = Techniques::FindCachedBoxDep2<SunFlareRes>(singlePass);
+            if (!doDirectBlur) {
+                Metal::RenderTargetView tempRTV[2];
+                Metal::ShaderResourceView tempSRV[2];
+
+                const unsigned resX = Tweakable("SunFlareResX", 128);
+                const unsigned resY = Tweakable("SunFlareResY", 32);
+                {
+                    using namespace BufferUploads;
+                    auto desc = CreateDesc(
+                        BindFlag::ShaderResource | BindFlag::RenderTarget, 0, GPUAccess::Read | GPUAccess::Write,
+                        TextureDesc::Plain2D(resX, resY, Metal::NativeFormat::R8_UNORM),
+                        "SunFlareTemp");
+                    for (unsigned c=0; c<(singlePass?1u:2u); ++c) {
+                        auto offscreen = GetBufferUploads().Transaction_Immediate(desc);
+                        tempSRV[c] = Metal::ShaderResourceView(offscreen->GetUnderlying());
+                        tempRTV[c] = Metal::RenderTargetView(offscreen->GetUnderlying());
+                    }
+                }
+
+                context->Bind(Techniques::CommonResources()._blendOpaque);
+                context->Bind(Metal::ViewportDesc(0.f, 0.f, float(resX), float(resY), 0.f, 1.f));
+
+                if (!singlePass) {
+                    context->Bind(MakeResourceList(tempRTV[1]), nullptr);
+                    Metal::ConstantBufferPacket constants[] = { settingsPkt };
+                    const Metal::ShaderResourceView* srvs[] = { &depthsSRV };
+                    res._toRadialUniforms.Apply(
+                        *context, 
+                        parserContext.GetGlobalUniformsStream(),
+                        Metal::UniformsStream(constants, srvs));
+                
+                    context->Bind(*res._toRadialShader);
+                    context->Draw(4);
+                    context->UnbindPS<Metal::ShaderResourceView>(3, 1);
+                }
+
+                {
+                    context->Bind(MakeResourceList(tempRTV[0]), nullptr);
+                    Metal::ConstantBufferPacket constants[] = { settingsPkt };
+                    const Metal::ShaderResourceView* srvs[] = { singlePass ? &depthsSRV : &tempSRV[1] };
+                    res._blurUniforms.Apply(
+                        *context, 
+                        parserContext.GetGlobalUniformsStream(),
+                        Metal::UniformsStream(constants, srvs));
+
+                    context->Bind(*res._blurShader);
+                    context->Draw(4);
+                    context->UnbindPS<Metal::ShaderResourceView>(3, 1);
+                }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+                
+                context->GetUnderlying()->OMSetRenderTargets(1, savedTargets.GetRenderTargets(), nullptr);
+                context->Bind(savedViewport);
+                context->Bind(Techniques::CommonResources()._blendStraightAlpha);
+
+                {
+                    Metal::ConstantBufferPacket constants[] = { settingsPkt };
+                    const Metal::ShaderResourceView* srvs[] = { &tempSRV[0] };
+                    res._commitUniforms.Apply(
+                        *context, 
+                        parserContext.GetGlobalUniformsStream(),
+                        Metal::UniformsStream(constants, srvs));
+
+                    context->Bind(*res._commitShader);
+                    context->Draw(4);
+                    context->UnbindPS<Metal::ShaderResourceView>(3, 1);
+                }
+
+            } else {
+
+                context->GetUnderlying()->OMSetRenderTargets(1, savedTargets.GetRenderTargets(), nullptr);
+                context->Bind(savedViewport);
+                context->Bind(Techniques::CommonResources()._blendStraightAlpha);
+
+                {
+                    Metal::ConstantBufferPacket constants[] = { settingsPkt };
+                    const Metal::ShaderResourceView* srvs[] = { &depthsSRV };
+                    res._directBlurUniforms.Apply(
+                        *context, 
+                        parserContext.GetGlobalUniformsStream(),
+                        Metal::UniformsStream(constants, srvs));
+
+                    context->Bind(*res._directBlurShader);
+                    context->Draw(4);
+                    context->UnbindPS<Metal::ShaderResourceView>(3, 1);
+                }
+                
+            }
         } 
         CATCH(const ::Assets::Exceptions::InvalidResource& e) { parserContext.Process(e); }
         CATCH(const ::Assets::Exceptions::PendingResource& e) { parserContext.Process(e); }
         CATCH_END
 
-        context->Bind(RenderCore::Metal::Topology::TriangleList);
+        context->Bind(Metal::Topology::TriangleList);
         savedTargets.ResetToOldTargets(context);
+        context->Bind(savedViewport);
     }
 }
 
--- Working/Game/xleres/Effects/occludingsunflare.sh
+++ Working/Game/xleres/Effects/occludingsunflare.sh
@@ -6,36 +6,108 @@
 
 #include "../CommonResources.h"
 #include "../Transform.h"
+#include "../TextureAlgorithm.h"
+#include "../Utility/MathConstants.h"
 
-Texture2D<float> DepthTexture;
+Texture2D<float> InputTexture;
 
 cbuffer Settings
 {
-    row_major float4x4 ProjectionMatrix;
-    float3 ViewSpaceSunPosition;
+    float2 ProjSpaceSunPosition;
+    float2 AspectCompensation;
 }
 
-float4 ps_sunflare(float4 pos : SV_Position, float3 startViewSpace : VIEWSPACEPOSITION) : SV_Target0
+float4 ps_sunflare_directblur(float4 pos : SV_Position, float2 projPos : PROJPOS) : SV_Target0
 {
-    // Walk between this point and the sun in view space, and find where the
-    // sun is occluded in the depth buffer. This will result in a fixed number
-    // of depth buffer samples per pixel
+        // Walk between this point and the sun in view space, and find where the
+        // sun is occluded in the depth buffer. This will result in a fixed number
+        // of depth buffer samples per pixel
+        // In the other version, we do a blur in radial coordinates first. This
+        // version doesn't require that blur (which will change the performance)
+        // but the result is more blocky and less smooth.
 
-    const uint steps = 8;
+    float DSq = length(projPos.xy - ProjSpaceSunPosition);
+    if (DSq > 0.75f)
+        return 0.0.xxxx;
+
+    const uint steps = 32;
     for (uint c=0; c<steps; ++c) {
-        float3 testPosition = lerp(startViewSpace, ViewSpaceSunPosition, c / float(steps));
-        float4 projected = mul(ProjectionMatrix, float4(testPosition, 1));
-        float2 tc = float2(.5f + .5f * projected.x / projected.w, .5f - .5f * projected.y / projected.w);
-        float depth = DepthTexture.SampleLevel(PointClampSampler, tc, 0);
-        if (depth < 1.f) return 0.0.xxxx;
+        float4 testPosition = float4(lerp(projPos.xy, ProjSpaceSunPosition, (c) / float(steps)), 0, 1);
+        float2 tc = float2(.5f + .5f * testPosition.x / testPosition.w, .5f - .5f * testPosition.y / testPosition.w);
+
+        float4 depths = InputTexture.Gather(PointClampSampler, tc);
+        // float A = dot(depths.xy, depths.zw);
+        // float T = 1.97f;
+        // if (A > T) return float4(10.0.xxx, (1.f-(DSq/.75f)) * saturate((A-T) / (2-T)));
+
+        float t = dot(depths == 1.f, 1.0.xxxx);
+        if (t > 0.f) return float4(10.0.xxx, (1.f-(DSq/.75f)) * (t/4.f));
     }
 
-    return 1.0.xxxx;
+    return 0.0.xxxx;
 }
 
-void vs_sunflare(uint vertexId : SV_VertexID, out float4 oPosition : SV_Position, out float3 oViewSpace : VIEWSPACEPOSITION)
+float4 ps_sunflare(float4 pos : SV_Position, float2 projPos : PROJPOS) : SV_Target0
+{
+    float2 off = projPos.xy - ProjSpaceSunPosition;
+    off /= AspectCompensation;
+
+    float d = length(off);
+    if (d > 1.f) discard;
+
+    float a = atan2(off.y, off.x);
+    return float4(10.0.xxx, (1.f - d) * InputTexture.SampleLevel(WrapUSampler, float2(a / (2.f * pi), d), 0));
+}
+
+void vs_sunflare(uint vertexId : SV_VertexID, out float4 oPosition : SV_Position, out float2 oProjSpace : PROJPOS)
 {
     float2 coord = float2((float)(vertexId / 2), (float)(vertexId % 2));
-    oViewSpace = FrustumCorners[vertexId].xyz;
     oPosition = float4(2.f * coord.x - 1.f, -2.f * coord.y + 1.f, 0.f, 1.f);
+    oProjSpace = oPosition.xy;
+}
+
+float2 ToCartesianTC(float2 radialTC)
+{
+    float2 cs;
+    sincos(2.f * pi * radialTC.x, cs.y, cs.x);
+    float2 source = ProjSpaceSunPosition + AspectCompensation * radialTC.y * cs;
+    return float2(.5f + .5f * source.x, .5f - .5f * source.y);
+}
+
+float ps_toradial(float4 pos : SV_Position, float2 projPos : PROJPOS) : SV_Target0
+{
+    float2 Q;
+    Q.x = .5f + .5f * projPos.x;
+    Q.y = .5f - .5f * projPos.y;
+
+    float4 depths = InputTexture.Gather(PointClampSampler, ToCartesianTC(Q));
+    return dot(depths == 1.f, 1.0.xxxx) / 4.f;
+}
+
+float ps_blur(float4 position : SV_Position, float2 texCoord : TEXCOORD0) : SV_Target0
+{
+    float result = 0.f;
+    uint baseY = 0; // uint(position.x)%4;
+    const uint sampleCount = 16;
+
+    float2 recipOutputDims = texCoord.xy / position.xy;
+
+    [unroll] for (uint c=0; c<sampleCount; c++) {
+        uint2 p;
+        p.x = position.x;
+        // p.x += float(c%4) / 2.f - 1.f;
+        p.y = baseY + uint(c / float(sampleCount) * float(position.y) + 0.5f);
+
+        #if (SINGLE_PASS==0)
+            float texSample = LoadFloat1(InputTexture, p, 0);
+        #else
+            float4 depths = InputTexture.Gather(PointClampSampler, ToCartesianTC(p * recipOutputDims));
+            float texSample = dot(depths == 1.f, 1.0.xxxx) / 4.f;
+        #endif
+
+        result += texSample;
+    }
+
+    return saturate(2.f * result / float(sampleCount));
+    // return saturate(result);
 }
