79bcde0dce6fc744d1a5913d286cd304e9f6957f
djewsbury <djewsbury@xlgames.com>
2015-11-24 15:29:03 +0900

    New structure for processing textures through gradle - much easier for multi-step processes (such as building skys)

--- Tools/AssetPath/buildSrc/src/main/groovy/TextureProcess.groovy
+++ Tools/AssetPath/buildSrc/src/main/groovy/TextureProcess.groovy
@@ -8,68 +8,101 @@ import org.gradle.api.tasks.*
 
 @groovy.transform.InheritConstructors
 class TextureProcessError extends java.lang.Exception
-{}
+{
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+class ProcessStep
+{
+	String getCommandLine(File input, File output)	{ return null; }
+	boolean doCheckErrorStream()					{ return true; }
+
+	boolean isIntermediate = false;
+
+	static Closure makeIntermediateName = null;
+    String asDestinationFileName(File i, String newExt, int intermediateIndex)
+    {
+        Object intName;
+        if (makeIntermediateName != null)	intName = makeIntermediateName(i);
+        else								intName = i.getPath();
+
+        return intName.substring(0, intName.lastIndexOf('.')) + (isIntermediate?"_${intermediateIndex}":"") + newExt;
+    }
+}
+
 class TextureTask extends DefaultTask
 {
-    @InputFile
     File input = null
 	File output = null
 
-    @OutputFile
-    File getOutputFile() 
-	{ 
-		if (output != null) return output;
-		return new File(asDestinationFileName(input)) 
+	List<ProcessStep> steps = new ArrayList<ProcessStep>();
+	Map<Integer, Integer> stepInputs = new HashMap<Integer, Integer>();
+
+	@InputFiles
+	List<File> getInputFiles()
+	{
+		def result = new ArrayList<File>();
+		if (input) result.add(input);
+		return result;
 	}
 
-    static Closure makeIntermediateName = null;
+    @OutputFiles
+    List<File> getOutputFiles()
+	{
+		def result = new ArrayList<File>();
+		for (int c=0; c<steps.size(); ++c) 
+			result.add(getOutputFile(c));
+		return result;
+	}
 
-    String asDestinationFileName(File i)
-    {
-        Object intName
-        if (TextureTask.makeIntermediateName != null)
-            intName = TextureTask.makeIntermediateName(i)
-        else intName = i.getPath()
-        return intName.substring(0, intName.lastIndexOf('.')) + ".dds";
-    }
+	File getOutputFile(int index)
+	{
+		if (index == steps.size()-1 && output) return output;
+		return new File(steps[index].asDestinationFileName(input, ".dds", index));
+	}
 
 	static void checkProcessErrorStream(Process process, String operation)
 	{
 		if (process.getErrorStream().available() > 0)
         {
-            BufferedReader reader =
-                new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            StringBuilder builder = new StringBuilder();
+            def reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            def builder = new StringBuilder();
             String line = null;
-            while ( (line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 builder.append(line);
                 builder.append(System.getProperty("line.separator"));
             }
 
-                // note that using TextureProcessError here causes a JVM exception in my
+                // Note that using TextureProcessError here causes a JVM exception in my
                 // version of the JDK. It's fixed in a newer version; but let's just use
                 // a basic exception, anyway
-            throw new java.lang.Exception("${builder.toString()} <<  >>")
+            throw new java.lang.Exception("${builder.toString()} << ${operation} >>")
         }
 	}
-
-	String getCommandLine(File input, File output) { return null; }
-	boolean doCheckErrorStream() { return true; }
 
 	@TaskAction
     void run()
 	{
-		def cmdLine = getCommandLine(input, getOutputFile());
-		println cmdLine
+			// Run each step one by one... Sometimes we create intermediate steps, which
+			// are stored in temporary files
 
-        def process = cmdLine.execute()
-        process.waitFor()
+		for (int c=0; c<steps.size(); ++c) {
+			def stepInput = input;
+			def t = stepInputs[c];
+			if (t) { stepInput = getOutputFile(t) }
+			else if (c!=0) { stepInput = getOutputFile(c-1) }
 
-		if (doCheckErrorStream())
-			checkProcessErrorStream(process, "while processing texture from ${input} to output file ${output}");
+			def stepOutput = getOutputFile(c)
+			def cmdLine = steps[c].getCommandLine(stepInput, stepOutput)
+			println cmdLine
+
+			def process = cmdLine.execute()
+			process.waitFor()
+
+			if (steps[c].doCheckErrorStream())
+				checkProcessErrorStream(process, "while processing texture from ${stepInput ? stepInput.getAbsolutePath() : "<no-input>"} to output file ${stepOutput.getAbsolutePath()}");
+		}
 	}
 
 	/*
@@ -105,7 +138,7 @@ class TextureTask extends DefaultTask
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-class TextureCompress extends TextureTask
+class TextureCompress extends ProcessStep
 {
     @Input
     String compressionMode = "bc1";
@@ -119,15 +152,18 @@ class TextureCompress extends TextureTask
     @Input
     boolean mips = true
 
+	@Input
+	boolean isNormalMap = false
+
 	String getCommandLine(File input, File output)
 	{
-		return "nvcompress -nocuda -silent -$compressionMode -$wrapping ${mips?"-mipfilter $mipFilter":"-nomips"} ${input.getAbsolutePath()} ${output.getAbsolutePath()}";
+		return "nvcompress -nocuda -silent -$compressionMode -$wrapping ${mips?"-mipfilter $mipFilter":"-nomips"} ${isNormalMap?"-normal":""} ${input.getAbsolutePath()} ${output.getAbsolutePath()}";
 	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-class TextureTransformTask extends TextureTask
+class TextureTransformStep extends ProcessStep
 {
 	String makeCommandLine(File output, String shader, String parameters)
 	{
@@ -135,7 +171,7 @@ class TextureTransformTask extends TextureTask
 	}
 }
 
-class EquirectToCube extends TextureTransformTask
+class EquirectToCube extends TextureTransformStep
 {
     @Input
     int faceSize = 512
@@ -157,7 +193,7 @@ class EquirectToCube extends TextureTransformTask
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-class GenericTextureGen extends TextureTransformTask
+class GenericTextureGen extends TextureTransformStep
 {
     @Input
     String format = "R16G16B16A16_FLOAT"
@@ -179,7 +215,7 @@ class GenericTextureGen extends TextureTransformTask
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-class CubeMapGen extends TextureTask
+class CubeMapGen extends ProcessStep
 {
 	String getCommandLine(File input, File output)
 	{
@@ -189,38 +225,68 @@ class CubeMapGen extends TextureTask
 	boolean doCheckErrorStream() { return false; }
 }
 
-class DiffuseCubeMapGen extends TextureTask
+class DiffuseCubeMapGen extends ProcessStep
 {
-    String asDestinationFileName(File i) 
+    String asDestinationFileName(File i, String newExt, int intermediateIndex)
 	{
-        def baseName = super.asDestinationFileName(i);
-        return baseName.substring(0, baseName.lastIndexOf('.')) + "_diffuse.dds";
+        def baseName = super.asDestinationFileName(i, newExt, intermediateIndex);
+        return baseName.substring(0, baseName.lastIndexOf('.')) + "_diffuse" + newExt;
     }
 
 	String getCommandLine(File input, File output)
 	{ 
-		"ModifiedCubeMapGen ${input.getAbsolutePath()} -exit -IrradianceCubemap:180 -exportCubeDDS -exportMipChain -edgeFixupWidth:0 -exportPixelFormat:A16B16G16R16F -exportSize=32 -exportFilename:${output.getAbsolutePath()}";
+		return "ModifiedCubeMapGen ${input.getAbsolutePath()} -exit -IrradianceCubemap:180 -exportCubeDDS -exportMipChain -edgeFixupWidth:0 -exportPixelFormat:A16B16G16R16F -exportSize:32 -exportFilename:${output.getAbsolutePath()}";
 	}
+
+	boolean doCheckErrorStream() { return false; }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 4bpp RGB default compression (with optional 1 bit alpha)
-class BC1 extends TextureCompress { BC1() { compressionMode = "bc1" } }
-class BC1a extends TextureCompress { BC1a() { compressionMode = "bc1a" } }
+class BC1 extends TextureTask		{ BC1() { steps.add(new TextureCompress(compressionMode:"bc1")) } }
+class BC1a extends TextureTask		{ BC1a() { steps.add(new TextureCompress(compressionMode:"bc1a")) } }
 
 // 8bpp RGBA
 // BC2: 4 bits/pixel quantized alpha
-// BC4: 4 bits/pixel interpolated alpha
-class BC2 extends TextureCompress { BC2() { compressionMode = "bc2" } }
-class BC3 extends TextureCompress { BC3() { compressionMode = "bc3" } }
+// BC3: 4 bits/pixel interpolated alpha
+class BC2 extends TextureTask		{ BC2() { steps.add(new TextureCompress(compressionMode:"bc2")) } }
+class BC3 extends TextureTask		{ BC3() { steps.add(new TextureCompress(compressionMode:"bc3")) } }
 
-class BC6 extends TextureCompress { BC6() { compressionMode = "bc6" } }
+class NormalMap extends TextureTask { NormalMap() { steps.add(new TextureCompress(compressionMode:"bc5", mipFilter:"box", isNormalMap:true)) } }
+
+class BC6 extends TextureTask		{ BC6() { steps.add(new TextureCompress(compressionMode:"bc6")) } }
 
 // 8bpp RGB or RGBA
 // high precision new format
-class BC7 extends TextureCompress { BC7() { compressionMode = "bc7" } }
+class BC7 extends TextureTask		{ BC7() { steps.add(new TextureCompress(compressionMode:"bc7")) } }
 
-class Lumi extends TextureCompress { Lumi() { compressionMode = "lumi" } }
+class Lumi extends TextureTask		{ Lumi() { steps.add(new TextureCompress(compressionMode:"lumi")) } }
 
-class RGB extends TextureCompress { RGB() { compressionMode = "rgb" } }
+class RGB extends TextureTask		{ RGB() { steps.add(new TextureCompress(compressionMode:"rgb")) } }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class EquiRectEnv extends TextureTask
+{
+	EquiRectEnv()
+	{
+		steps.add(new TextureCompress(compressionMode:"rgb", mips:false, isIntermediate:true));
+		steps.add(new EquirectToCube(isIntermediate:true));
+		steps.add(new CubeMapGen());
+		steps.add(new DiffuseCubeMapGen());
+		stepInputs[3] = 1;
+	}
+}
+
+class HemiEquiRectEnv extends TextureTask
+{
+	HemiEquiRectEnv()
+	{
+		steps.add(new TextureCompress(compressionMode:"rgb", mips:false, isIntermediate:true));
+		steps.add(new EquirectToCube(isIntermediate:true, shader:"hemi"));
+		steps.add(new CubeMapGen());
+		steps.add(new DiffuseCubeMapGen());
+		stepInputs[3] = 1;
+	}
+}
