00d1b96f570fe434afddee64167f61d58f2a8828
djewsbury <djewsbury@xlgames.com>
2016-01-31 20:24:46 +0900

    Integrated Intel ISPC BC6H compressor - TextureTransform will now prefer to use this compressor when compressing 16 or 32 bit float inputs to BC6H_UF16 format - much, much faster and also higher quality output - always using "very-slow" compressor currently

--- Samples/TextureTransform/TexCompress.cpp
+++ Samples/TextureTransform/TexCompress.cpp
@@ -11,7 +11,11 @@
 #include "../../Utility/ParameterBox.h"
 #include "../../Utility/Conversion.h"
 #include "../../Utility/StringUtils.h"
+#include "../../Foreign/half-1.9.2/include/half.hpp"
 #include "../../Foreign/DirectXTex/DirectXTex/DirectXTex.h"
+#include "../../Foreign/ISPCTex/ispc_texcomp.h"
+
+#undef max
 
 namespace TextureTransform
 {
@@ -73,6 +77,148 @@ namespace TextureTransform
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+    static DirectX::ScratchImage PeformCompression_DXTex(
+        const BufferUploads::TextureDesc& srcDesc, 
+        BufferUploads::DataPacket& pkt,
+        RenderCore::Metal::NativeFormat::Enum dstFormat)
+    {
+        // use SRGB compression for formats where it makes sense.
+        const auto flags = DirectX::TEX_COMPRESS_SRGB | DirectX::TEX_COMPRESS_PARALLEL;
+
+        auto metaData = AsMetadata(srcDesc);
+        auto images = BuildImages(metaData, pkt);
+
+        DirectX::ScratchImage scratchImage;
+        auto hresult = DirectX::Compress(
+            AsPointer(images.cbegin()), images.size(),
+            metaData, 
+            RenderCore::Metal::AsDXGIFormat(dstFormat),
+            flags, 0.5f, scratchImage);
+        if (!SUCCEEDED(hresult))
+            Throw(::Exceptions::BasicLabel("Error while performing compression. Check formats"));
+
+        return std::move(scratchImage);
+    }
+
+    inline uint32 FloatBits(float f) 
+    {
+        union { float f; unsigned i; } u;
+        u.f = f;
+        return u.i;
+    }
+
+#if 0
+    static unsigned short AsUnsignedFloat16_Fast(float input)
+    {
+        // We can't use the "half" library for this -- because that only supports signed floats
+
+            //
+            //      See stack overflow article:
+            //          http://stackoverflow.com/questions/3026441/float32-to-float16
+            //
+            //      He suggests either using a table lookup or vectorising
+            //      this code for further optimisation.
+            //
+        unsigned int fltInt32 = FloatBits(input);
+        
+        unsigned short tmp = (fltInt32 >> 23) & 0xff;
+        tmp = (tmp - 0x70) & ((unsigned int)((int)(0x70 - tmp) >> 4) >> 27);
+        
+        unsigned short fltInt16 = tmp << 11;
+        fltInt16 |= (fltInt32 >> 12) & 0x7ff;
+        
+        // note -- no rounding performed!
+        return fltInt16;    
+    }
+#endif
+
+#if 1
+    static unsigned short AsSignedFloat16_Fast(float input)
+    {
+        // unsigned int fltInt32 = FloatBits(input);
+        // 
+        // unsigned short fltInt16 = (fltInt32 >> 31) << 5;
+        // 
+        // unsigned short tmp = (fltInt32 >> 23) & 0xff;
+        // tmp = (tmp - 0x70) & ((unsigned int)((int)(0x70 - tmp) >> 4) >> 27);
+        // 
+        // fltInt16 = (fltInt16 | tmp) << 10;
+        // fltInt16 |= (fltInt32 >> 13) & 0x3ff;
+        // 
+        // return fltInt16;
+        return half_float::detail::float2half<std::round_to_nearest>(input);
+    }
+#endif
+
+    static DirectX::ScratchImage PeformCompression_IntelBC6H_UF16(
+        const BufferUploads::TextureDesc& srcDesc,
+        BufferUploads::DataPacket& pkt)
+    {
+        // We're going to the intel ispc compressor to create a BC6H_UF16 texture.
+        // This code was originally distributed as a part of some sample code... So it's
+        // not very flexible. Our version only takes 16 bit float RGBA inputs. Since the
+        // output is also a 16bit float format, this makes sense. But we'll have to do
+        // the conversion to 16 bit on this side.
+        // We need to manually split up the input pkt into smaller blocks and do all of 
+        // the thread management ourselves.
+        bc6h_enc_settings settings;
+        GetProfile_bc6h_veryslow(&settings);
+
+        DirectX::ScratchImage result;
+        auto scratchMetadata = AsMetadata(srcDesc);
+        scratchMetadata.format = DXGI_FORMAT_BC6H_UF16;
+        result.Initialize(scratchMetadata);
+
+        for (unsigned a=0; a<srcDesc._arrayCount; ++a)
+            for (unsigned m=0; m<srcDesc._mipCount; ++m) {
+                auto subRes = BufferUploads::DataPacket::TexSubRes(m, a);
+                auto* srcData = pkt.GetData(subRes);
+                auto srcPitches = pkt.GetPitches(subRes);
+
+                rgba_surface src 
+                {
+                    (uint8_t*)srcData, 
+                    std::max(4u, srcDesc._width >> m),
+                    std::max(4u, srcDesc._height >> m),
+                    srcPitches._rowPitch
+                };
+
+                // todo -- we may need to copy smaller mipmaps into an extra buffer
+                //      (to make sure there is always at least 1 full 4x4 block)
+
+                std::unique_ptr<uint8[]> midwayBuffer;
+                if (srcDesc._nativePixelFormat == RenderCore::Metal::NativeFormat::R32G32B32A32_FLOAT
+                    || srcDesc._nativePixelFormat == RenderCore::Metal::NativeFormat::R32G32B32_FLOAT) {
+                    
+                    const unsigned srcPixelPitch = (srcDesc._nativePixelFormat == RenderCore::Metal::NativeFormat::R32G32B32A32_FLOAT?4:3)*4;
+                    const unsigned dstPixelPitch = 8;
+                    
+                    midwayBuffer = std::make_unique<uint8[]>(src.width*src.height*dstPixelPitch);
+                    std::memset(midwayBuffer.get(), 0, src.width*src.height*dstPixelPitch);
+                    
+                    // convert 32 bit float -> unsigned 16 bit float
+                    // Note there is no sign bit. We want 5 exponent bits + 11 mantissa bits
+                    for (unsigned y=0; y<unsigned(src.height); ++y)
+                        for (unsigned x = 0; x<unsigned(src.width); ++x) {
+                            auto* s = (float*)PtrAdd(srcData, (y*srcPitches._rowPitch) + x*srcPixelPitch);
+                            auto* d = (uint16*)PtrAdd(midwayBuffer.get(), (y*unsigned(src.width)*dstPixelPitch) + x*dstPixelPitch);
+                            d[0] = AsSignedFloat16_Fast(s[0]);
+                            d[1] = AsSignedFloat16_Fast(s[1]);
+                            d[2] = AsSignedFloat16_Fast(s[2]);
+                            d[3] = 0;   // alpha not supported
+                        }
+
+                    src.ptr = midwayBuffer.get();
+                    src.stride = src.width*dstPixelPitch;
+                }
+
+                auto* image = result.GetImage(m, a, 0);
+                CompressBlocksBC6H(&src, image->pixels, &settings);
+            }
+
+        return std::move(result);
+    }
+
     TextureResult CompressTexture(
         const BufferUploads::TextureDesc& desc, 
         const ParameterBox& parameters)
@@ -92,26 +238,25 @@ namespace TextureTransform
         if (state != ::Assets::AssetState::Ready)
             Throw(::Exceptions::BasicLabel("Failure while loading input texture"));
 
-        // use SRGB compression for formats where it makes sense.
-        const auto flags = DirectX::TEX_COMPRESS_SRGB | DirectX::TEX_COMPRESS_PARALLEL;
-
         // interpret input as a SRGB format (if possible)
         // todo -- handle SRGB input flags
         srcDesc._nativePixelFormat = (unsigned)RenderCore::Metal::AsSRGBFormat(
             RenderCore::Metal::NativeFormat::Enum(srcDesc._nativePixelFormat));
 
-        auto metaData = AsMetadata(srcDesc);
-        auto images = BuildImages(metaData, *pkt);
+        auto dstFormat = RenderCore::Metal::NativeFormat::Enum(desc._nativePixelFormat);
+        auto srcFormat = RenderCore::Metal::NativeFormat::Enum(srcDesc._nativePixelFormat);
 
         DirectX::ScratchImage scratchImage;
-        auto hresult = DirectX::Compress(
-            AsPointer(images.cbegin()), images.size(),
-            metaData, 
-            RenderCore::Metal::AsDXGIFormat(
-                RenderCore::Metal::NativeFormat::Enum(desc._nativePixelFormat)),
-            flags, 0.5f, scratchImage);
-        if (!SUCCEEDED(hresult))
-            Throw(::Exceptions::BasicLabel("Error while performing compression. Check formats"));
+        if (dstFormat == RenderCore::Metal::NativeFormat::BC6H_UF16
+            && (srcFormat == RenderCore::Metal::NativeFormat::R16G16B16A16_FLOAT
+                || srcFormat == RenderCore::Metal::NativeFormat::R32G32B32A32_FLOAT
+                || srcFormat == RenderCore::Metal::NativeFormat::R32G32B32_FLOAT)) {
+            // When writing to BC6H_UF16, prefer to use the intel compressor. This compressor
+            // is both much faster and more accurate. However, it doesn't support a wide range of
+            // input formats -- our version will only work with 
+            scratchImage = PeformCompression_IntelBC6H_UF16(srcDesc, *pkt);
+        } else
+            scratchImage = PeformCompression_DXTex(srcDesc, *pkt, dstFormat);
 
         // We need to convert the output to a TextureResult to pass back. 
         // This requires creating a special case DataPacket that wraps the ScratchImage object
