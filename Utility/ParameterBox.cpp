92cec14faffdfb0088757509a7699abaa108fd79
David Jewsbury <djewsbury@pocketgems.com>
2017-06-25 16:39:32 -0700

    Minor fix to avoid an assertion when a key in a parameter box has a zero-length value

--- Utility/ParameterBox.cpp
+++ Utility/ParameterBox.cpp
@@ -806,9 +806,11 @@ namespace Utility
                 i2->second += valueSize;
             }
 
-            _values.insert(
-                _values.cbegin()+dstOffsets.second, 
-                (uint8*)value, (uint8*)PtrAdd(value, valueSize));
+            if (valueSize) {
+                _values.insert(
+                    _values.cbegin()+dstOffsets.second, 
+                    (uint8*)value, (uint8*)PtrAdd(value, valueSize));
+            }
             _names.insert(
                 _names.cbegin()+dstOffsets.first, 
                 name, &name[nameLength]);
