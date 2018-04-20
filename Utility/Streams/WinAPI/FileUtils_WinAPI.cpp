686c8d72b1e9c7549a2d9c2c4a7fb25028323c2e
Thiago Goulart <thiago@pocketgems.com>
2018-04-20 15:59:58 -0700

    Re-enable TryGetDesc on win32.

--- Utility/Streams/WinAPI/FileUtils_WinAPI.cpp
+++ Utility/Streams/WinAPI/FileUtils_WinAPI.cpp
@@ -468,7 +468,6 @@ namespace Utility
 			return { uint64(attribs.nFileSizeHigh) << 32 | uint64(attribs.nFileSizeLow), AsUInt64(attribs.ftLastWriteTime), AsUInt64(attribs.ftLastAccessTime) };
 		}
 
-#if 0
 		std::optional<FileAttributes> TryGetFileAttributes(const utf8 filename[])
 		{
 			WIN32_FILE_ATTRIBUTE_DATA attribs;
@@ -488,7 +487,6 @@ namespace Utility
 				&attribs);
 			return (result) ? AsFileAttributes(attribs) : std::optional<FileAttributes>{};
 		}
-#endif
 
 		std::vector<std::string> FindFiles(const std::string& searchPath, FindFilesFilter::BitField filter)
 		{
