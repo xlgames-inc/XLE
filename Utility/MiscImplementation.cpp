60667530d8e42384f4ef1b5c6d392fabad6c8c87
David Jewsbury <djewsbury@gmail.com>
2018-07-07 18:57:33 -0700

    VariantArray utility class

--- Utility/MiscImplementation.cpp
+++ Utility/MiscImplementation.cpp
@@ -1,10 +1,10 @@
-// Copyright 2015 XLGAMES Inc.
-//
 // Distributed under the MIT License (See
 // accompanying file "LICENSE" or the website
 // http://www.opensource.org/licenses/mit-license.php)
 
 #include "MemoryUtils.h"
+#include "VariantUtils.h"
+#include "PtrUtils.h"
 
 namespace Utility
 {
@@ -36,6 +36,115 @@ namespace Utility
         }
 
         return result;
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void VariantArray::const_iterator::operator++()
+    {
+        auto size = _entryIterator->_size;
+        ++_entryIterator;
+        _dataStoreIterator += size;
+    }
+
+    bool operator==(const VariantArray::const_iterator& lhs, const VariantArray::const_iterator& rhs)
+    {
+        assert((lhs._entryIterator == rhs._entryIterator) == (lhs._dataStoreIterator == rhs._dataStoreIterator));
+        return lhs._entryIterator == rhs._entryIterator;
+    }
+
+    bool operator!=(const VariantArray::const_iterator& lhs, const VariantArray::const_iterator& rhs)
+    {
+        assert((lhs._entryIterator == rhs._entryIterator) == (lhs._dataStoreIterator == rhs._dataStoreIterator));
+        return lhs._entryIterator != rhs._entryIterator;
+    }
+
+    VariantArray::const_iterator::const_iterator(
+        std::vector<Entry>::const_iterator entryIterator,
+        const uint8_t* dataStoreIterator)
+    : _entryIterator(entryIterator), _dataStoreIterator(dataStoreIterator)
+    {}
+
+    VariantArray::const_iterator::~const_iterator() {}
+
+    auto VariantArray::begin() const -> const_iterator
+    {
+        return const_iterator{_entries.begin(), _dataStore.get()};
+    }
+
+    auto VariantArray::end() const -> const_iterator
+    {
+        return const_iterator{_entries.end(), &_dataStore[_dataStoreSize]};
+    }
+
+    void VariantArray::reserve(size_t byteCount)
+    {
+        if (_dataStoreAllocated >= byteCount)
+            return;
+        assert(_dataStoreSize <= byteCount);
+
+        auto newDataStore = std::make_unique<uint8_t[]>(byteCount);
+        auto* dstPtr = (void*)newDataStore.get();
+        auto* srcPtr = (void*)_dataStore.get();
+        for (auto i=_entries.begin(); i!=_entries.end(); ++i) {
+            assert(srcPtr < PtrAdd(_dataStore.get(), _dataStoreSize));
+            assert(dstPtr < PtrAdd(newDataStore.get(), byteCount));
+
+            // Move objects into the new data store, and call destructor on old store
+            (*i->_moveFn)(dstPtr, srcPtr);
+            (*i->_destroyFn)(srcPtr);
+            dstPtr = PtrAdd(dstPtr, i->_size);
+            srcPtr = PtrAdd(srcPtr, i->_size);
+        }
+        _dataStore = std::move(newDataStore);
+        _dataStoreAllocated = byteCount;
+    }
+
+    void VariantArray::clear()
+    {
+        auto* srcPtr = (void*)_dataStore.get();
+        for (auto i=_entries.begin(); i!=_entries.end(); ++i) {
+            (*i->_destroyFn)(srcPtr);
+            srcPtr = PtrAdd(srcPtr, i->_size);
+        }
+        _dataStoreSize = 0;
+        _entries.clear();
+    }
+
+    VariantArray::VariantArray(VariantArray&& moveFrom)
+    : _dataStore(std::move(moveFrom._dataStore))
+    , _dataStoreSize(moveFrom._dataStoreSize)
+    , _dataStoreAllocated(moveFrom._dataStoreAllocated)
+    , _entries(std::move(moveFrom._entries))
+    {
+        moveFrom._dataStoreSize = 0;
+        moveFrom._dataStoreAllocated = 0;
+    }
+
+    VariantArray& VariantArray::operator=(VariantArray&& moveFrom)
+    {
+        _dataStore = std::move(moveFrom._dataStore);
+        _dataStoreSize = moveFrom._dataStoreSize;
+        _dataStoreAllocated = moveFrom._dataStoreAllocated;
+        _entries = std::move(moveFrom._entries);
+        moveFrom._dataStoreSize = 0;
+        moveFrom._dataStoreAllocated = 0;
+        return *this;
+    }
+
+    VariantArray::VariantArray()
+    {
+        _dataStoreSize = 0;
+        _dataStoreAllocated = 0;
+    }
+
+    VariantArray::~VariantArray()
+    {
+        auto* srcPtr = (void*)_dataStore.get();
+        for (auto i=_entries.begin(); i!=_entries.end(); ++i) {
+            (*i->_destroyFn)(srcPtr);
+            srcPtr = PtrAdd(srcPtr, i->_size);
+        }
     }
 }
 
---
+++ Utility/VariantUtils.h
@@ -0,0 +1,152 @@
+// Distributed under the MIT License (See
+// accompanying file "LICENSE" or the website
+// http://www.opensource.org/licenses/mit-license.php)
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+namespace Utility
+{
+	/// <summary>A dynamically sized array containing variant types</summary>
+	/// This array can contain arbitrary counts of different types of objects in contiguous memory.
+	///
+	/// Typically all of the types stored in any given array will have some common base class
+	/// (such as an abstract interface base class). In this way, clients can iterate through
+	/// the array and operate on each element polymorphically.
+	///
+	/// Elements are added to the array using the Allocate() method (which takes a template
+	/// parameter for the type to allocate). The array will resize it's internal memory using
+	/// an std::vector-like allocation scheme.
+	///
+	/// Use the begin() and end() functions to iterate through the array. The incrementing
+	/// operator on the returned iterator will always advance one element forward, even if
+	/// the elements are of varying sizes.
+    class VariantArray
+    {
+    private:
+        using MoveFn = void(*)(void*, void*);
+        using DestroyFn = void(*)(void*);
+        class Entry
+        {
+        public:
+            MoveFn      _moveFn;
+            DestroyFn   _destroyFn;
+            size_t      _size;
+        };
+
+    public:
+        template<typename Type>
+            Type* Allocate(size_t count=1);
+
+        class const_iterator
+        {
+        public:
+            void operator++();
+            friend bool operator==(const const_iterator& lhs, const const_iterator& rhs);
+            friend bool operator!=(const const_iterator& lhs, const const_iterator& rhs);
+
+            const void* operator->() const { return _dataStoreIterator; }
+            const void* get() const { return _dataStoreIterator; }
+            size_t GetItemSize() const { return _entryIterator->_size; }
+
+            const_iterator(
+                std::vector<Entry>::const_iterator entryIterator,
+                const uint8_t* dataStoreIterator);
+            ~const_iterator();
+
+        private:
+            std::vector<Entry>::const_iterator _entryIterator;
+            const uint8_t* _dataStoreIterator;
+        };
+
+        const_iterator begin() const;
+        const_iterator end() const;
+        bool empty() const { return _entries.empty(); }
+        void reserve(size_t byteCount);
+        void reserve_entries(size_t count) { _entries.reserve(count); }
+        void clear();
+
+        VariantArray();
+        ~VariantArray();
+        VariantArray(VariantArray&&);
+        VariantArray& operator=(VariantArray&&);
+
+    private:
+        std::unique_ptr<uint8_t[]> _dataStore;
+        size_t _dataStoreSize;
+        size_t _dataStoreAllocated;
+        std::vector<Entry> _entries;
+
+        template<typename Type> static Entry MakeEntry();
+        template<typename Type> static void MoveFnWrapper(void*, void*);
+        template<typename Type> static void DestroyFnWrapper(void*);
+    };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    template<typename Type>
+        Type* VariantArray::Allocate(size_t count)
+    {
+        size_t startSize = _dataStoreSize;
+        size_t allocationRequired = count*sizeof(Type);
+        if ((startSize + allocationRequired) > _dataStoreAllocated) {
+            size_t newAllocation = startSize + allocationRequired;
+            newAllocation += newAllocation / 2;
+
+            auto newDataStore = std::make_unique<uint8_t[]>(newAllocation);
+            auto* dstPtr = (void*)newDataStore.get();
+            auto* srcPtr = (void*)_dataStore.get();
+            for (auto i=_entries.begin(); i!=_entries.end(); ++i) {
+                assert(srcPtr < PtrAdd(_dataStore.get(), _dataStoreSize));
+                assert(dstPtr < PtrAdd(newDataStore.get(), newAllocation));
+
+                // Move objects into the new data store, and call destructor on old store
+                (*i->_moveFn)(dstPtr, srcPtr);
+                (*i->_destroyFn)(srcPtr);
+                dstPtr = PtrAdd(dstPtr, i->_size);
+                srcPtr = PtrAdd(srcPtr, i->_size);
+            }
+            _dataStore = std::move(newDataStore);
+            _dataStoreAllocated = newAllocation;
+        }
+
+        // Initialize new elements, with default constructor
+        auto it = startSize;
+        for (unsigned c=0; c<count; ++c, it+=sizeof(Type)) {
+            new(&_dataStore[it]) Type();
+        }
+
+        // Update entries array
+        _dataStoreSize = startSize + allocationRequired;
+        _entries.reserve(_entries.size()+count);
+        for (unsigned c=0; c<count; ++c)
+            _entries.push_back(MakeEntry<Type>());
+
+        return (Type*)&_dataStore[startSize];
+    }
+
+    template<typename Type>
+        auto VariantArray::MakeEntry() -> Entry
+    {
+        return {
+            &MoveFnWrapper<Type>,
+            &DestroyFnWrapper<Type>,
+            sizeof(Type),
+        };
+    }
+
+    template<typename Type>
+        void VariantArray::MoveFnWrapper(void* lhs, void* rhs)
+    {
+        *(Type*)lhs = std::move(*(Type*)rhs);
+    }
+
+    template<typename Type>
+        void VariantArray::DestroyFnWrapper(void* obj)
+    {
+        ((Type*)obj)->~Type();
+    }
+}
+
