8b4fa7c12ff36c0e4438aeedf64d35bf71d06c1a
Yiteng Guo <yitengguo@pocketgems.com>
2018-07-17 15:15:30 -0700

    log exception_ptr message

--- Utility/ExceptionLogging.h
+++ Utility/ExceptionLogging.h
@@ -9,25 +9,47 @@
 #include "../Core/Exceptions.h"
 #include <ostream>
 
-namespace Utility
+// Use namespace Exceptions and std instead of Utility here,
+// because we should use the namespace which the rhs of the stream operator is in.
+namespace Exceptions
 {
     template<typename CharType, typename CharTraits>
-        std::basic_ostream<CharType, CharTraits>& operator<<(
-            std::basic_ostream<CharType, CharTraits>& stream,
-            const ::Exceptions::BasicLabel& exception)
-        {
-            stream << exception.what();
-            return stream;
-        }
-
-    template<typename CharType, typename CharTraits>
-        std::basic_ostream<CharType, CharTraits>& operator<<(
-            std::basic_ostream<CharType, CharTraits>& stream,
-            const std::exception& exception)
-        {
-            stream << exception.what();
-            return stream;
-        }
+    std::basic_ostream<CharType, CharTraits> &operator<<(
+            std::basic_ostream<CharType, CharTraits> &stream,
+            const ::Exceptions::BasicLabel &exception)
+    {
+        stream << exception.what();
+        return stream;
+    }
 }
 
-using namespace Utility;
+namespace std
+{
+    template<typename CharType, typename CharTraits>
+    std::basic_ostream<CharType, CharTraits> &operator<<(
+            std::basic_ostream<CharType, CharTraits> &stream,
+            const std::exception &exception)
+    {
+        stream << exception.what();
+        return stream;
+    }
+
+    template<typename CharType, typename CharTraits>
+    std::basic_ostream<CharType, CharTraits> &operator<<(
+            std::basic_ostream<CharType, CharTraits> &stream,
+            const std::exception_ptr &exception_ptr)
+    {
+        TRY {
+            std::rethrow_exception(exception_ptr);
+        } CATCH (const std::exception &e) {
+            stream << e.what();
+        } CATCH (const std::string &e) {
+            stream << e;
+        } CATCH (const char *e) {
+            stream << e;
+        } CATCH (...) {
+            stream << "Unknown Exception";
+        } CATCH_END
+        return stream;
+    }
+}
