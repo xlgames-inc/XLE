5b16a28a45715498bf4f158908107a69a5e44f10
David Jewsbury <djewsbury@gmail.com>
2018-08-19 12:04:54 -0700

    Converter all of the example node graph files to the graph syntax format

--- Working/Game/xleres/Objects/Example/ProcWood/WoodModulator001.sh
+++ Working/Game/xleres/Objects/Example/ProcWood/WoodModulator001.sh
@@ -1,231 +1,19 @@
 // CompoundDocument:1
-#include "xleres/System/Prefix.h"
+import basic = "xleres/nodes/basic.sh"
 
-#include "xleres/Nodes/Basic.sh"
-
-
-void WoodModulator001(float value, float period, out float frac : SV_Target0, out float int : SV_Target1)
+void main(out float frac, out float int)
 {
-	float Output_67_result;
-	Output_67_result = Divide1( value, period );
-
-	float Output_66_result;
-	Output_66_result = Round1( value );
-
-	float Output_63_result;
-	Output_63_result = Subtract1( Output_67_result, Output_66_result );
-
-	float Output_64_result;
-	Output_64_result = Multiply1( Output_66_result, period );
-
-	float Output_65_result;
-	Output_65_result = Multiply1( Output_63_result, period );
-
-	frac = Output_65_result;
-	int = Output_64_result;
-
+	node node_0 = [[visualNode1]]basic::Divide1(lhs:"value", rhs:"period");
+	node node_1 = [[visualNode2]]basic::Round1(lhs:node_0.result, multipleOf:".5");
+	frac = [[visualNode5]]basic::Multiply1(lhs:[[visualNode3]]basic::Subtract1(lhs:node_0.result, rhs:node_1.result).result, rhs:"period").result;
+	int = [[visualNode4]]basic::Multiply1(lhs:node_1.result, rhs:"period").result;
 }
-/* <<Chunk:NodeGraph:WoodModulator001>>--(
-<?xml version="1.0" encoding="utf-8"?>
-<NodeGraph xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.datacontract.org/2004/07/ShaderPatcherLayer">
-	<ConstantConnections>
-		<ConstantConnection>
-			<OutputNodeID>67</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<Value>&lt;value&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>67</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>66</OutputNodeID>
-			<OutputParameterName>multipleOf</OutputParameterName>
-			<Value>.5</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>64</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>65</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-	</ConstantConnections>
-	<InputParameterConnections />
-	<NodeConnections>
-		<NodeConnection>
-			<OutputNodeID>66</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>67</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>63</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>67</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>64</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>66</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>63</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<InputNodeID>66</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>65</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>63</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-	</NodeConnections>
-	<Nodes>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Divide1</FragmentArchiveName>
-			<NodeId>67</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>1</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Round1</FragmentArchiveName>
-			<NodeId>66</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>2</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Subtract1</FragmentArchiveName>
-			<NodeId>63</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>3</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Multiply1</FragmentArchiveName>
-			<NodeId>64</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>4</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Multiply1</FragmentArchiveName>
-			<NodeId>65</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>5</VisualNodeId>
-		</Node>
-	</Nodes>
-	<OutputParameterConnections>
-		<OutputParameterConnection>
-			<InputNodeID>65</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<Name>frac</Name>
-			<Semantic></Semantic>
-			<Type>float</Type>
-			<VisualNodeId>0</VisualNodeId>
-		</OutputParameterConnection>
-		<OutputParameterConnection>
-			<InputNodeID>64</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<Name>int</Name>
-			<Semantic></Semantic>
-			<Type>float</Type>
-			<VisualNodeId>0</VisualNodeId>
-		</OutputParameterConnection>
-	</OutputParameterConnections>
-	<PreviewSettingsObjects>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>1</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>2</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>3</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>4</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>5</VisualNodeId>
-		</PreviewSettings>
-	</PreviewSettingsObjects>
-	<VisualNodes>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>2234</d4p1:x>
-				<d4p1:y>421</d4p1:y>
-			</Location>
-			<State>Normal</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1331</d4p1:x>
-				<d4p1:y>371</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1540</d4p1:x>
-				<d4p1:y>433</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1704</d4p1:x>
-				<d4p1:y>345</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1921</d4p1:x>
-				<d4p1:y>475</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1916</d4p1:x>
-				<d4p1:y>391</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-	</VisualNodes>
-</NodeGraph>
-)-- */
+attributes visualNode0(X:"2234.000000", Y:"421.000000", State:"Normal");
+attributes visualNode1(PreviewGeometry:"sphere", X:"1331.000000", Y:"371.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode2(PreviewGeometry:"sphere", X:"1540.000000", Y:"433.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode3(PreviewGeometry:"sphere", X:"1704.000000", Y:"345.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode4(PreviewGeometry:"sphere", X:"1921.000000", Y:"475.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode5(PreviewGeometry:"sphere", X:"1916.000000", Y:"391.000000", State:"Collapsed", OutputToVisualize:"");
 /* <<Chunk:NodeGraphContext:WoodModulator001>>--(
 <?xml version="1.0" encoding="utf-8"?>
 <NodeGraphContext xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.datacontract.org/2004/07/ShaderPatcherLayer">
@@ -234,3 +22,8 @@ void WoodModulator001(float value, float period, out float frac : SV_Target0, ou
 	<Variables xmlns:d2p1="http://schemas.microsoft.com/2003/10/Serialization/Arrays" />
 </NodeGraphContext>
 )-- */
+/* <<Chunk:CBLayout:main>>--(
+
+
+)--*/
+
--- Working/Game/xleres/Objects/Example/ProcWood/WoodModulator002.sh
+++ Working/Game/xleres/Objects/Example/ProcWood/WoodModulator002.sh
@@ -1,226 +1,19 @@
 // CompoundDocument:1
-#include "xleres/System/Prefix.h"
+import basic = "xleres/nodes/basic.sh"
 
-#include "xleres/Nodes/Basic.sh"
-
-
-void WoodModulator002(float value, float period, out float int : SV_Target0, out float frac : SV_Target1)
+void main(out float int, out float frac)
 {
-	float Output_69_result;
-	Output_69_result = Divide1( value, period );
-
-	float Output_70_result;
-	Output_70_result = Round1( Output_69_result );
-
-	float Output_71_result;
-	Output_71_result = Multiply1( Output_70_result, period );
-
-	float Output_73_result;
-	Output_73_result = Subtract1( Output_69_result, Output_70_result );
-
-	float Output_72_result;
-	Output_72_result = Multiply1( Output_73_result, period );
-
-	int = Output_71_result;
-	frac = Output_72_result;
-
+	node node_0 = [[visualNode0]]basic::Divide1(lhs:"value", rhs:"period");
+	node node_1 = [[visualNode3]]basic::Round1(value:node_0.result);
+	int = [[visualNode4]]basic::Multiply1(lhs:node_1.result, rhs:"period").result;
+	frac = [[visualNode5]]basic::Multiply1(lhs:[[visualNode2]]basic::Subtract1(lhs:node_0.result, rhs:node_1.result).result, rhs:"period").result;
 }
-/* <<Chunk:NodeGraph:WoodModulator002>>--(
-<?xml version="1.0" encoding="utf-8"?>
-<NodeGraph xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.datacontract.org/2004/07/ShaderPatcherLayer">
-	<ConstantConnections>
-		<ConstantConnection>
-			<OutputNodeID>69</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<Value>&lt;value&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>69</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>71</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-		<ConstantConnection>
-			<OutputNodeID>72</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<Value>&lt;period&gt;</Value>
-		</ConstantConnection>
-	</ConstantConnections>
-	<InputParameterConnections />
-	<NodeConnections>
-		<NodeConnection>
-			<OutputNodeID>73</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>69</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>70</OutputNodeID>
-			<OutputParameterName>value</OutputParameterName>
-			<InputNodeID>69</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>72</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>73</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>73</OutputNodeID>
-			<OutputParameterName>rhs</OutputParameterName>
-			<InputNodeID>70</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-		<NodeConnection>
-			<OutputNodeID>71</OutputNodeID>
-			<OutputParameterName>lhs</OutputParameterName>
-			<InputNodeID>70</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<InputType>float</InputType>
-			<OutputType>float</OutputType>
-			<Semantic i:nil="true" />
-		</NodeConnection>
-	</NodeConnections>
-	<Nodes>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Divide1</FragmentArchiveName>
-			<NodeId>69</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>0</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Subtract1</FragmentArchiveName>
-			<NodeId>73</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>2</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Round1</FragmentArchiveName>
-			<NodeId>70</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>3</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Multiply1</FragmentArchiveName>
-			<NodeId>71</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>4</VisualNodeId>
-		</Node>
-		<Node>
-			<FragmentArchiveName>xleres/Nodes/Basic.sh:Multiply1</FragmentArchiveName>
-			<NodeId>72</NodeId>
-			<NodeType>Procedure</NodeType>
-			<VisualNodeId>5</VisualNodeId>
-		</Node>
-	</Nodes>
-	<OutputParameterConnections>
-		<OutputParameterConnection>
-			<InputNodeID>71</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<Name>int</Name>
-			<Semantic></Semantic>
-			<Type>float</Type>
-			<VisualNodeId>1</VisualNodeId>
-		</OutputParameterConnection>
-		<OutputParameterConnection>
-			<InputNodeID>72</InputNodeID>
-			<InputParameterName>result</InputParameterName>
-			<Name>frac</Name>
-			<Semantic></Semantic>
-			<Type>float</Type>
-			<VisualNodeId>1</VisualNodeId>
-		</OutputParameterConnection>
-	</OutputParameterConnections>
-	<PreviewSettingsObjects>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>0</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>2</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>3</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>4</VisualNodeId>
-		</PreviewSettings>
-		<PreviewSettings>
-			<Geometry>Sphere</Geometry>
-			<OutputToVisualize></OutputToVisualize>
-			<VisualNodeId>5</VisualNodeId>
-		</PreviewSettings>
-	</PreviewSettingsObjects>
-	<VisualNodes>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>157</d4p1:x>
-				<d4p1:y>257</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>1009</d4p1:x>
-				<d4p1:y>317</d4p1:y>
-			</Location>
-			<State>Normal</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>548</d4p1:x>
-				<d4p1:y>268</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>365</d4p1:x>
-				<d4p1:y>348</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>677</d4p1:x>
-				<d4p1:y>377</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-		<VisualNode>
-			<Location xmlns:d4p1="http://schemas.datacontract.org/2004/07/System.Drawing">
-				<d4p1:x>717</d4p1:x>
-				<d4p1:y>279</d4p1:y>
-			</Location>
-			<State>Collapsed</State>
-		</VisualNode>
-	</VisualNodes>
-</NodeGraph>
-)-- */
+attributes visualNode0(PreviewGeometry:"sphere", X:"157.000000", Y:"257.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode1(X:"1009.000000", Y:"317.000000", State:"Normal");
+attributes visualNode2(PreviewGeometry:"sphere", X:"548.000000", Y:"268.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode3(PreviewGeometry:"sphere", X:"365.000000", Y:"348.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode4(PreviewGeometry:"sphere", X:"677.000000", Y:"377.000000", State:"Collapsed", OutputToVisualize:"");
+attributes visualNode5(PreviewGeometry:"sphere", X:"717.000000", Y:"279.000000", State:"Collapsed", OutputToVisualize:"");
 /* <<Chunk:NodeGraphContext:WoodModulator002>>--(
 <?xml version="1.0" encoding="utf-8"?>
 <NodeGraphContext xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.datacontract.org/2004/07/ShaderPatcherLayer">
@@ -234,3 +27,8 @@ void WoodModulator002(float value, float period, out float int : SV_Target0, out
 	</Variables>
 </NodeGraphContext>
 )-- */
+/* <<Chunk:CBLayout:main>>--(
+
+
+)--*/
+
