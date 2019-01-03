9f7a7d35db21ba01997e126582e768c1858085ba
David Jewsbury <djewsbury@pocketgems.com>
2019-01-03 13:28:15 -0800

    Allow SelectConfiguration to be compiled for pure C files - only the clib & STL sections are specific to C++, so we can allow for some flexibility here

--- Core/SelectConfiguration.h
+++ Core/SelectConfiguration.h
@@ -6,8 +6,6 @@
 
 #pragma once
 
-#include <ciso646>      // (for standard library detection)
-
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 #define CLIBRARIES_MSVC     1
@@ -32,17 +30,23 @@
     #error "Cannot determine current compiler type. Platform unsupported!"
 #endif
 
-#if (_LIBCPP_VERSION)
-    #define CLIBRARIES_ACTIVE   CLIBRARIES_LIBCPP
-    #define STL_ACTIVE          STL_LIBCPP
-#elif defined(__GLIBCXX__)
-    #define CLIBRARIES_ACTIVE   CLIBRARIES_GCC
-    #define STL_ACTIVE          STL_GCC
-#elif defined(_CPPLIB_VER)
-    #define CLIBRARIES_ACTIVE   CLIBRARIES_MSVC
-    #define STL_ACTIVE          STL_MSVC
-#else
-    #error "Cannot determine C libraries and STL type. Platform unsupported!"
+#if defined(__cplusplus)
+
+    #include <ciso646>      // (for standard library detection)
+
+    #if (_LIBCPP_VERSION)
+        #define CLIBRARIES_ACTIVE   CLIBRARIES_LIBCPP
+        #define STL_ACTIVE          STL_LIBCPP
+    #elif defined(__GLIBCXX__)
+        #define CLIBRARIES_ACTIVE   CLIBRARIES_GCC
+        #define STL_ACTIVE          STL_GCC
+    #elif defined(_CPPLIB_VER)
+        #define CLIBRARIES_ACTIVE   CLIBRARIES_MSVC
+        #define STL_ACTIVE          STL_MSVC
+    #else
+        #error "Cannot determine C++ libraries and STL type. Platform unsupported!"
+    #endif
+
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
