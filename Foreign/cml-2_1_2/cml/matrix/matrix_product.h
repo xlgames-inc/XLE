565dd31150cb1d576357e04169a07561e3df39c7
Ken Domke <ken@pocketgems.com>
2019-05-17 17:23:09 -0700

    Upgrading from cml-1.0.2 to cml-2.1.2

---
+++ Foreign/cml-2_1_2/cml/matrix/matrix_product.h
@@ -0,0 +1,35 @@
+/* -*- C++ -*- ------------------------------------------------------------
+ @@COPYRIGHT@@
+ *-----------------------------------------------------------------------*/
+/** @file
+ */
+
+#pragma once
+
+#ifndef	cml_matrix_matrix_product_h
+#define	cml_matrix_matrix_product_h
+
+#include <cml/matrix/readable_matrix.h>
+#include <cml/matrix/promotion.h>
+
+namespace cml {
+
+/** Multiply two matrices, and return the result as a temporary. */
+template<class Sub1, class Sub2,
+  enable_if_matrix_t<Sub1>* = nullptr,
+  enable_if_matrix_t<Sub2>* = nullptr>
+auto operator*(Sub1&& sub1, Sub2&& sub2)
+-> matrix_inner_product_promote_t<
+  actual_operand_type_of_t<decltype(sub1)>,
+  actual_operand_type_of_t<decltype(sub2)>>;
+
+} // namespace cml
+
+#define __CML_MATRIX_MATRIX_PRODUCT_TPP
+#include <cml/matrix/matrix_product.tpp>
+#undef __CML_MATRIX_MATRIX_PRODUCT_TPP
+
+#endif
+
+// -------------------------------------------------------------------------
+// vim:ft=cpp:sw=2
