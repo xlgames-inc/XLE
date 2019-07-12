37675e1ae8eaad8c9c4b73685601e2f0badf6bf6
David Jewsbury <djewsbury@pocketgems.com>
2019-07-12 16:10:09 -0700

    Conversions for a broader range of attribute formats for Apple Metal layer

--- RenderCore/AppleMetal/Metal/Format.cpp
+++ RenderCore/AppleMetal/Metal/Format.cpp
@@ -349,14 +349,45 @@ namespace RenderCore { namespace Metal_AppleMetal
     MTLVertexFormat AsMTLVertexFormat(RenderCore::Format fmt)
     {
         switch (fmt) {
+            case Format::R32_FLOAT: return MTLVertexFormatFloat;
             case Format::R32G32_FLOAT: return MTLVertexFormatFloat2;
             case Format::R32G32B32_FLOAT: return MTLVertexFormatFloat3;
             case Format::R32G32B32A32_FLOAT: return MTLVertexFormatFloat4;
 
+            case Format::R32_SINT: return MTLVertexFormatInt;
+            case Format::R32G32_SINT: return MTLVertexFormatInt2;
+            case Format::R32G32B32_SINT: return MTLVertexFormatInt3;
+            case Format::R32G32B32A32_SINT: return MTLVertexFormatInt4;
+
+            case Format::R32_UINT: return MTLVertexFormatUInt;
+            case Format::R32G32_UINT: return MTLVertexFormatUInt2;
+            case Format::R32G32B32_UINT: return MTLVertexFormatUInt3;
+            case Format::R32G32B32A32_UINT: return MTLVertexFormatUInt4;
+
+            case Format::R8G8_UNORM: return MTLVertexFormatUChar2Normalized;
+            case Format::R8G8B8_UNORM: return MTLVertexFormatUChar3Normalized;
             case Format::R8G8B8A8_UNORM: return MTLVertexFormatUChar4Normalized;
+
+            case Format::R8G8_SNORM: return MTLVertexFormatChar2Normalized;
+            case Format::R8G8B8_SNORM: return MTLVertexFormatChar3Normalized;
+            case Format::R8G8B8A8_SNORM: return MTLVertexFormatChar4Normalized;
+
+            case Format::R16G16_UNORM: return MTLVertexFormatUShort2Normalized;
+            case Format::R16G16B16A16_UNORM: return MTLVertexFormatUShort4Normalized;
+
+            case Format::R16G16_SNORM: return MTLVertexFormatShort2Normalized;
             case Format::R16G16B16A16_SNORM: return MTLVertexFormatShort4Normalized;
 
+            case Format::R16G16_FLOAT: return MTLVertexFormatHalf2;
             case Format::R16G16B16A16_FLOAT: return MTLVertexFormatHalf4;
+
+            case Format::R8G8_UINT: return MTLVertexFormatUChar2;
+            case Format::R8G8B8_UINT: return MTLVertexFormatUChar3;
+            case Format::R8G8B8A8_UINT: return MTLVertexFormatUChar4;
+
+            case Format::R8G8_SINT: return MTLVertexFormatChar2;
+            case Format::R8G8B8_SINT: return MTLVertexFormatChar3;
+            case Format::R8G8B8A8_SINT: return MTLVertexFormatChar4;
 
             default: break;
         }
