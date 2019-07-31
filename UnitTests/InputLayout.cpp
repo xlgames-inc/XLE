81eb23d5121562ddce277f7b2c44d55e7af1ec07
David Jewsbury <djewsbury@pocketgems.com>
2019-07-31 15:45:21 -0700

    Improved the input layout unit test, so more of it's code can be reused by other tests

--- UnitTests/InputLayout.cpp
+++ UnitTests/InputLayout.cpp
@@ -20,6 +20,10 @@
 
 namespace UnitTests
 {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            //    T E S T   S H A D E R S
+
 #if GFXAPI_TARGET == GFXAPI_APPLEMETAL
 	static const char vsText[] = R"(
 		
@@ -38,7 +42,7 @@ namespace UnitTests
 		vertex RasterizerData vertexShader(AAPLVertex v_in [[stage_in]])
 		{
 			RasterizerData out;
-			out.clipSpacePosition = float4(v_in.position.x / 50.f, v_in.position.y / 50.f, 0.0f, 1.0f);
+			out.clipSpacePosition = v_in.position;
 			out.color = v_in.color;
 			return out;
 		}
@@ -58,6 +62,91 @@ namespace UnitTests
 	)";
 #endif
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            //    T E S T   I N P U T   D A T A
+
+    class VertexPC
+    {
+    public:
+        Float4      _position;
+        unsigned    _color;
+    };
+
+    static const unsigned fixedColors[] = { 0xff7f7f7fu, 0xff007f7fu, 0xff7f0000u };
+
+    static VertexPC inputVerticesPC[] = {
+        VertexPC { Float4 {  -0.25f, -0.5f,  0.0f,  1.0f }, fixedColors[0] },
+        VertexPC { Float4 {  -0.33f,  0.1f,  0.0f,  1.0f }, fixedColors[0] },
+        VertexPC { Float4 {   0.33f, -0.2f,  0.0f,  1.0f }, fixedColors[0] },
+
+        VertexPC { Float4 { -0.1f, -0.7f, 0.0f, 1.0f }, fixedColors[1] },
+        VertexPC { Float4 {  0.5f, -0.4f, 0.0f, 1.0f }, fixedColors[1] },
+        VertexPC { Float4 {  0.8f,  0.8f, 0.0f, 1.0f }, fixedColors[1] },
+
+        VertexPC { Float4 { 0.25f, -0.6f, 0.0f, 1.0f }, fixedColors[2] },
+        VertexPC { Float4 { 0.75f,  0.1f, 0.0f, 1.0f }, fixedColors[2] },
+        VertexPC { Float4 { 0.4f,   0.7f, 0.0f, 1.0f }, fixedColors[2] }
+    };
+
+    static RenderCore::InputElementDesc inputElePC[] = {
+        RenderCore::InputElementDesc { "position", 0, RenderCore::Format::R32G32B32A32_FLOAT },
+        RenderCore::InputElementDesc { "color", 0, RenderCore::Format::R8G8B8A8_UNORM }
+    };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            //    U T I L I T Y    F N S
+
+
+    std::shared_ptr<RenderCore::IResource> CreateVB(RenderCore::IDevice& device, IteratorRange<const void*> data)
+    {
+        using namespace RenderCore;
+        return device.CreateResource(
+            CreateDesc(
+                BindFlag::VertexBuffer, 0, GPUAccess::Read,
+                LinearBufferDesc::Create((unsigned)data.size()),
+                "vertexBuffer"),
+            [data](SubResourceId) -> SubResourceInitData { return SubResourceInitData { data }; });
+    }
+
+    class UnitTestFBHelper
+    {
+    public:
+        RenderCore::Techniques::AttachmentPool _namedResources;
+        RenderCore::Techniques::FrameBufferPool _frameBufferPool;
+        RenderCore::Techniques::TechniqueContext _techniqueContext;
+        RenderCore::Techniques::ParsingContext _parsingContext;     // careful init-order rules
+
+        std::shared_ptr<RenderCore::IResource> _target;
+        RenderCore::Techniques::RenderPassInstance _rpi;
+
+        UnitTestFBHelper(RenderCore::IDevice& device, RenderCore::IThreadContext& threadContext, const RenderCore::ResourceDesc& targetDesc)
+        : _parsingContext(_techniqueContext, &_namedResources, &_frameBufferPool)     // careful init-order rules
+        {
+            auto* metalThreadContext = (RenderCore::ImplAppleMetal::ThreadContext*)threadContext.QueryInterface(typeid(RenderCore::ImplAppleMetal::ThreadContext).hash_code());
+            if (metalThreadContext)
+                metalThreadContext->BeginHeadlessFrame();
+
+            _target = device.CreateResource(targetDesc);
+            _rpi = RenderCore::Techniques::RenderPassToPresentationTarget(threadContext, _target, _parsingContext, RenderCore::LoadStore::Clear);
+            _threadContext = &threadContext;
+        }
+
+        ~UnitTestFBHelper()
+        {
+            if (_threadContext) {
+                auto* metalThreadContext = (RenderCore::ImplAppleMetal::ThreadContext*)_threadContext->QueryInterface(typeid(RenderCore::ImplAppleMetal::ThreadContext).hash_code());
+                if (metalThreadContext)
+                    metalThreadContext->EndHeadlessFrame();
+            }
+        }
+
+    private:
+        RenderCore::IThreadContext* _threadContext;
+    };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            //    C O D E
+
     TEST_CLASS(InputLayout)
 	{
 	public:
@@ -66,7 +155,6 @@ namespace UnitTests
 		TEST_CLASS_INITIALIZE(Startup)
 		{
 			_testHelper = std::make_unique<MetalTestHelper>(RenderCore::UnderlyingAPI::AppleMetal);
-			// ::Assets::MainFileSystem::GetMountingTree()->Mount(u("xleres"), ::Assets::CreateFileSystem_OS(u("Game/xleres")));
 		}
 
 		TEST_CLASS_CLEANUP(Shutdown)
@@ -77,98 +165,57 @@ namespace UnitTests
 		TEST_METHOD(BasicBinding)
 		{
 			using namespace RenderCore;
-			auto& device = *_testHelper->_device;
-			auto threadContext = device.GetImmediateContext();
-			auto& metalContext = *Metal::DeviceContext::Get(*threadContext);
+			auto threadContext = _testHelper->_device->GetImmediateContext();
 
-			auto vs = _testHelper->MakeShader(vsText, "vs_5_0");
-			auto ps = _testHelper->MakeShader(psText, "ps_5_0");
-
+			auto vs = _testHelper->MakeShader(vsText, "vs_*");
+			auto ps = _testHelper->MakeShader(psText, "ps_*");
 			Metal::ShaderProgram shaderProgram(Metal::GetObjectFactory(), vs, ps);
 
             auto targetDesc = CreateDesc(
                 BindFlag::RenderTarget, CPUAccess::Read, GPUAccess::Write,
                 TextureDesc::Plain2D(1024, 1024, Format::R8G8B8A8_UNORM),
                 "temporary-out");
-			auto target = device.CreateResource(targetDesc);
 
-            std::vector<uint8_t> data;
-            const unsigned fixedColor = 0xff7f7f7fu;
+            UnitTestFBHelper fbHelper(*_testHelper->_device, *threadContext, targetDesc);
 
-            auto* metalThreadContext = (RenderCore::ImplAppleMetal::ThreadContext*)threadContext->QueryInterface(typeid(RenderCore::ImplAppleMetal::ThreadContext).hash_code());
-            if (metalThreadContext) {
-                metalThreadContext->BeginHeadlessFrame();
-            }
-
-            RenderCore::Techniques::AttachmentPool namedResources;
-            RenderCore::Techniques::FrameBufferPool frameBufferPool;
-			RenderCore::Techniques::TechniqueContext techniqueContext;
-			RenderCore::Techniques::ParsingContext parsingContext(techniqueContext, &namedResources, &frameBufferPool);
-			auto rpi = RenderCore::Techniques::RenderPassToPresentationTarget(*threadContext, target, parsingContext, LoadStore::Clear);
-
-			////////////////////////////////////////////////////////////////////////
+			////////////////////////////////////////////////////////////////////////////////////////
 			{
-                class Vertex
-                {
-                public:
-                    Float4      _position;
-                    unsigned    _color;
-                };
-				Vertex inputVertices[] = {
-					Vertex { Float4 {  1.0f,  2.0f,  3.0f,  4.0f }, fixedColor },
-					Vertex { Float4 {  5.0f,  6.0f,  7.0f,  8.0f }, fixedColor },
-					Vertex { Float4 { 11.0f, 12.0f, 13.0f, 14.0f }, fixedColor },
+				auto vertexBuffer = CreateVB(*_testHelper->_device, MakeIteratorRange(inputVerticesPC));
 
-					Vertex { Float4 { 15.0f, 16.0f, 17.0f, 18.0f }, fixedColor },
-					Vertex { Float4 { 21.0f, 22.0f, 23.0f, 24.0f }, fixedColor },
-					Vertex { Float4 { 25.0f, 26.0f, 27.0f, 28.0f }, fixedColor },
-
-					Vertex { Float4 { 31.0f, 32.0f, 33.0f, 34.0f }, fixedColor },
-					Vertex { Float4 { 35.0f, 36.0f, 37.0f, 38.0f }, fixedColor },
-					Vertex { Float4 { 41.0f, 42.0f, 43.0f, 44.0f }, fixedColor }
-				};
-
-				auto vertexBuffer = device.CreateResource(
-					CreateDesc(
-						BindFlag::VertexBuffer, 0, GPUAccess::Read,
-						LinearBufferDesc::Create(sizeof(inputVertices)),
-						"vertexBuffer"),
-					[inputVertices](SubResourceId) -> SubResourceInitData { return SubResourceInitData { MakeIteratorRange(inputVertices) }; });
-				InputElementDesc inputEle[] = {
-                    InputElementDesc { "position", 0, Format::R32G32B32A32_FLOAT },
-                    InputElementDesc { "color", 0, Format::R8G8B8A8_UNORM }
-                };
-				Metal::BoundInputLayout inputLayout(MakeIteratorRange(inputEle), shaderProgram);
+				Metal::BoundInputLayout inputLayout(MakeIteratorRange(inputElePC), shaderProgram);
 
 				VertexBufferView vbv { vertexBuffer.get() };
-				inputLayout.Apply(metalContext, MakeIteratorRange(&vbv, &vbv+1));
+				auto& metalContext = *Metal::DeviceContext::Get(*threadContext);
+                inputLayout.Apply(metalContext, MakeIteratorRange(&vbv, &vbv+1));
 
 				metalContext.Bind(Metal::ViewportDesc{ 0.f, 0.f, (float)targetDesc._textureDesc._width, (float)targetDesc._textureDesc._height });
 				metalContext.Bind(shaderProgram);
 				metalContext.Bind(Topology::TriangleList);
-				metalContext.Draw(dimof(inputVertices));				
+				metalContext.Draw(dimof(inputVerticesPC));
 			}
-			////////////////////////////////////////////////////////////////////////
+			////////////////////////////////////////////////////////////////////////////////////////
 
-			rpi = {};
+			fbHelper._rpi = {};     // end RPI
 
-            data = target->ReadBack(*threadContext);
-
-            if (metalThreadContext) {
-                metalThreadContext->EndHeadlessFrame();
-            }
+            auto data = fbHelper._target->ReadBack(*threadContext);
 
             // We're expecting all pixels either to be black or fixedColor
             unsigned blackPixels = 0;
-            unsigned coloredPixels = 0;
+            unsigned coloredPixels[dimof(fixedColors)] = {};
             unsigned otherPixels = 0;
 
             Assert::AreEqual(data.size(), (size_t)RenderCore::ByteCount(targetDesc));
-            for (auto p:MakeIteratorRange((unsigned*)AsPointer(data.begin()), (unsigned*)AsPointer(data.end()))) {
-                if (p == 0xff000000) ++blackPixels;
-                else if (p == fixedColor) ++coloredPixels;
-                else ++otherPixels;
+            auto pixels = MakeIteratorRange((unsigned*)AsPointer(data.begin()), (unsigned*)AsPointer(data.end()));
+            for (auto p:pixels) {
+                if (p == 0xff000000) { ++blackPixels; continue; }
+
+                for (unsigned c=0; c<dimof(fixedColors); ++c) {
+                    if (p == fixedColors[c])
+                        ++coloredPixels[c];
+                }
             }
+            otherPixels = (unsigned)pixels.size() - blackPixels;
+            for (unsigned c=0; c<dimof(fixedColors); ++c) otherPixels -= coloredPixels[c];
 
             Assert::AreEqual(otherPixels, 0u);
 		}
