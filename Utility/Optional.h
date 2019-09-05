c3b736fcbea8c937eb142aa4c181411a6fe0dddc
Max McArthur <46458612+maxmcarthur-pg@users.noreply.github.com>
2019-09-04 17:27:54 -0700

    XCode 11 deprecates <experimental/optional>; switch to optional-lite for now, while we stall upgrading to C++17 (<optional> requires C++17) (#29)

--- Utility/Optional.h
+++ Utility/Optional.h
@@ -6,19 +6,6 @@
 
 #if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) && !ANDROID
 	#include <optional>
-#elif __has_include(<experimental/optional>) && !ANDROID
-    #if defined(_LIBCPP_WARN_ON_DEPRECATED_EXPERIMENTAL_HEADER)
-        #undef _LIBCPP_WARN_ON_DEPRECATED_EXPERIMENTAL_HEADER
-    #endif
-    #include <experimental/optional>
-    namespace std
-    {
-        // Expose the std::optional type. This is part of the C++17 standard, but only provided
-        // as part of the "std::experimental" namespace in our version of the C++ library
-        template <typename T>
-            using optional = std::experimental::optional<T>;
-         constexpr std::experimental::nullopt_t nullopt = std::experimental::nullopt;
-    }
 #else
      #include "../Foreign/optional-lite/include/nonstd/optional.hpp"
      namespace std
