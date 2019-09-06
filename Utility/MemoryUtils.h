bfe984a483e538a1f66241734ef4977ed3da834a
Ken Domke <ken@pocketgems.com>
2019-09-06 14:14:02 -0700

    Adding default_init_allocator which will not use value initialization, but will just use default initialization.  This is convenient for resizable containers where one does not need the contents to be fully initialized.

--- Utility/MemoryUtils.h
+++ Utility/MemoryUtils.h
@@ -186,6 +186,35 @@ namespace Utility
         return result;
     }
 
+        ////////////   D E F A U L T   I N I T I A L I Z A T I O N   A L L O C A T O R   ////////////
+
+    // See https://stackoverflow.com/questions/21028299/is-this-behavior-of-vectorresizesize-type-n-under-c11-and-boost-container/21028912#21028912
+    // Allocator adaptor that interposes construct() calls to
+    // convert value initialization into default initialization.
+    template <typename T, typename A=std::allocator<T>>
+    class default_init_allocator : public A {
+        typedef std::allocator_traits<A> a_t;
+    public:
+        template <typename U> struct rebind {
+            using other =
+            default_init_allocator<
+            U, typename a_t::template rebind_alloc<U>
+            >;
+        };
+        using A::A;
+
+        template <typename U>
+        void construct(U* ptr)
+        noexcept(std::is_nothrow_default_constructible<U>::value) {
+            ::new(static_cast<void*>(ptr)) U;
+        }
+        template <typename U, typename...Args>
+        void construct(U* ptr, Args&&... args) {
+            a_t::construct(static_cast<A&>(*this),
+                           ptr, std::forward<Args>(args)...);
+        }
+    };
+
 }
 
 using namespace Utility;
