a07ca95802543deaa55953510092c31a7d2d0126
David Jewsbury <djewsbury@gmail.com>
2019-12-24 19:48:56 -0800

    Separated the list of modules out into includeable cmake file
    Useful for putting together separate projects

---
+++ Solutions/CMake/modules.cmake
@@ -0,0 +1,20 @@
+
+add_subdirectory(${MAIN_CMAKE_DIR}/../../Math Math)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../Utility Utility)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../Assets Assets)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../PlatformRig PlatformRig)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../ConsoleRig ConsoleRig)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../RenderCore RenderCore)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../RenderCore/Metal RenderCoreMetal)
+
+add_subdirectory(${MAIN_CMAKE_DIR}/../../RenderCore/Techniques RenderCoreTechniques)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../RenderCore/Assets RenderCoreAssets)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../RenderOverlays RenderOverlays)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../SceneEngine SceneEngine)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../BufferUploads BufferUploads)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../FixedFunctionModel FixedFunctionModel)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../ShaderParser ShaderParser)
+
+add_subdirectory(${MAIN_CMAKE_DIR}/../../Foreign ForeignMisc)
+add_subdirectory(${MAIN_CMAKE_DIR}/../../Foreign/tinyxml2-master tinyxml2)
+
