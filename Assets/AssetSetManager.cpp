7102d2e6e197e359183ca13167152d3c8493d57b
David Jewsbury <djewsbury@gmail.com>
2020-01-13 00:47:10 -0800

    Improved handling when trying to create a new AssetSet while iterating through the array of asset sets - When using polling callbacks, we sometimes have to create a new type of AssetSet during the AssetSetManager::OnFrameBarrier call - this approach will handle this situation safely; while also minimizing overhead in the far more common in which no new sets are created

--- Assets/AssetSetManager.cpp
+++ Assets/AssetSetManager.cpp
@@ -19,26 +19,36 @@ namespace Assets
 	{
 	public:
 		std::vector<std::pair<size_t, std::unique_ptr<IDefaultAssetHeap>>> _sets;
+		std::vector<std::pair<size_t, std::unique_ptr<IDefaultAssetHeap>>> _setsPendingIteration;
 		Threading::RecursiveMutex _lock;
+
+		bool _inIterationOperation = false;
 	};
 
 	IDefaultAssetHeap * AssetSetManager::GetSetForTypeCode(size_t typeCode)
 	{
 		auto i = LowerBound(_pimpl->_sets, typeCode);
 		if (i != _pimpl->_sets.end() && i->first == typeCode)
-		return i->second.get();
+			return i->second.get();
+
+		i = LowerBound(_pimpl->_setsPendingIteration, typeCode);
+		if (i != _pimpl->_setsPendingIteration.end() && i->first == typeCode)
+			return i->second.get();
+
 		return nullptr;
 	}
 
 	void AssetSetManager::Add(size_t typeCode, std::unique_ptr<IDefaultAssetHeap>&& set)
 	{
-		auto i = LowerBound(_pimpl->_sets, typeCode);
-		assert(i == _pimpl->_sets.end() || i->first != typeCode);
-		_pimpl->_sets.insert(
-			i,
-			std::make_pair(
-				typeCode,
-				std::forward<std::unique_ptr<IDefaultAssetHeap>>(set)));
+		auto* target = &_pimpl->_sets;
+		// We can't modify "_sets" while we're iterating through it.
+		// As a simple solution, just keep the these new sets in a pending list
+		if (_pimpl->_inIterationOperation)
+			target = &_pimpl->_setsPendingIteration;
+
+		auto i = LowerBound(*target, typeCode);
+		assert(i == target->end() || i->first != typeCode);
+		target->insert(i, std::make_pair(typeCode, std::move(set)));
 	}
 
     void AssetSetManager::Clear()
@@ -65,6 +75,7 @@ namespace Assets
 
     const IDefaultAssetHeap* AssetSetManager::GetAssetSet(unsigned index)
     {
+		// -- note -- pending sets can't be returned
         return _pimpl->_sets[index].second.get();
     }
 
@@ -81,14 +92,20 @@ namespace Assets
 	void AssetSetManager::OnFrameBarrier()
 	{
 		ScopedLock(_pimpl->_lock);
+		_pimpl->_inIterationOperation = true;
 		for (auto&set:_pimpl->_sets)
 			set.second->OnFrameBarrier();
+
+		// If we queued up any new sets to add to the main list, handle them now
+		for (auto&set:_pimpl->_setsPendingIteration)
+			_pimpl->_sets.insert(LowerBound(_pimpl->_sets, set.first), std::move(set));
+		_pimpl->_setsPendingIteration.clear();
+		_pimpl->_inIterationOperation = false;
 	}
 
     AssetSetManager::AssetSetManager()
     {
-        auto pimpl = std::make_unique<Pimpl>();
-        _pimpl = std::move(pimpl);
+        _pimpl = std::make_unique<Pimpl>();
     }
 
     AssetSetManager::~AssetSetManager()
