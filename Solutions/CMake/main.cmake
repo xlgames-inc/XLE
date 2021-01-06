ccbb2059de24c93b66d00af3987740b500f2919f
David Jewsbury <djewsbury@gmail.com>
2021-01-06 22:22:59 +1000

    Some maintainence for the cmake configuration files. Moving a little further towards just using cmake for building on everything

--- Solutions/CMake/main.cmake
+++ Solutions/CMake/main.cmake
@@ -1,61 +1,66 @@
 # setup basic XLE_DIR & FOREIGN_DIR macros
 get_filename_component(XLE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
 get_filename_component(FOREIGN_DIR "${XLE_DIR}/Foreign/" ABSOLUTE)
+get_filename_component(XLE_MAIN_CMAKE_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 
-macro(BasicLibrary LibName Src)
-	source_group("" FILES ${Src})       # Push all files into the root folder
-    add_library(${LibName} STATIC ${Src})
+macro(configure_xle_library LibName)
     if (MSVC)
-        set_target_properties(${LibName} PROPERTIES VS_USER_PROPS "${MAIN_CMAKE_DIR}/Main.props")
+        set_target_properties(${LibName} PROPERTIES VS_USER_PROPS "${XLE_MAIN_CMAKE_DIR}/Main.props")
     endif ()
     target_include_directories(${LibName} PRIVATE ${XLE_DIR})
+    target_compile_features(${LibName} PUBLIC cxx_std_17)
+    set_property(TARGET ${LibName} PROPERTY CXX_EXTENSIONS OFF)
+
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        target_compile_definitions(${LibName} PUBLIC -DXL_DEBUG -D_DEBUG)
+    elseif ( (CMAKE_BUILD_TYPE STREQUAL "Release") OR (CMAKE_BUILD_TYPE STREQUAL "MinSizeRel") OR (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo"))
+        target_compile_definitions(${LibName} PUBLIC -DXL_RELEASE -DNDEBUG)
+    endif ()
 endmacro()
 
-macro(BasicExecutable ExeName Src)
-    source_group("" FILES ${Src})       # Push all files into the root folder
-    add_executable(${ExeName} ${Src})
-
+macro(configure_xle_executable ExeName)
     if (MSVC)
         # The CMake Visual Studio generator has a hack that disables the LinkLibraryDependencies setting in
         # the output project (see cmGlobalVisualStudio8Generator::NeedLinkLibraryDependencies) unless there are
         # external project dependencies. It's frustrating because we absolutely need that on. To get around the
         # problem, we'll link in a dummy external project that just contains nothing. This causes cmake to
         # enable the LinkLibraryDependencies flag, and hopefully has no other side effects.
-        include_external_msproject(generator_dummy ${MAIN_CMAKE_DIR}/generator_dummy.vcxproj)
+        include_external_msproject(generator_dummy ${XLE_MAIN_CMAKE_DIR}/generator_dummy.vcxproj)
         add_dependencies(${ExeName} generator_dummy)
-        set_target_properties(${ExeName} PROPERTIES VS_USER_PROPS "${MAIN_CMAKE_DIR}/Main.props")
+        set_target_properties(${ExeName} PROPERTIES VS_USER_PROPS "${XLE_MAIN_CMAKE_DIR}/Main.props")
     endif (MSVC)
 
     target_include_directories(${ExeName} PRIVATE ${XLE_DIR})
+    target_compile_features(${ExeName} PUBLIC cxx_std_17)
+    set_property(TARGET ${ExeName} PROPERTY CXX_EXTENSIONS OFF)
 
-    if (NOT WIN32)
-        target_link_libraries(${ExeName} pthread)
-    else ()
-        target_link_libraries(${ExeName} wsock32.lib ws2_32.lib)
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        target_compile_definitions(${ExeName} PUBLIC -DXL_DEBUG -D_DEBUG)
+    elseif ( (CMAKE_BUILD_TYPE STREQUAL "Release") OR (CMAKE_BUILD_TYPE STREQUAL "MinSizeRel") OR (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo"))
+        target_compile_definitions(${ExeName} PUBLIC -DXL_RELEASE -DNDEBUG)
     endif ()
 endmacro()
 
-macro(BasicDLL DllName Src)
-    source_group("" FILES ${Src})       # Push all files into the root folder
-    add_library(${DllName} SHARED ${Src})
-
+macro(configure_xle_dll DllName)
     if (MSVC)
         # The CMake Visual Studio generator has a hack that disables the LinkLibraryDependencies setting in
         # the output project (see cmGlobalVisualStudio8Generator::NeedLinkLibraryDependencies) unless there are
         # external project dependencies. It's frustrating because we absolutely need that on. To get around the
         # problem, we'll link in a dummy external project that just contains nothing. This causes cmake to
         # enable the LinkLibraryDependencies flag, and hopefully has no other side effects.
-        include_external_msproject(generator_dummy ${MAIN_CMAKE_DIR}/generator_dummy.vcxproj)
+        include_external_msproject(generator_dummy ${XLE_MAIN_CMAKE_DIR}/generator_dummy.vcxproj)
         add_dependencies(${DllName} generator_dummy)
-        set_target_properties(${DllName} PROPERTIES VS_USER_PROPS "${MAIN_CMAKE_DIR}/Main.props")
+        set_target_properties(${DllName} PROPERTIES VS_USER_PROPS "${XLE_MAIN_CMAKE_DIR}/Main.props")
     endif (MSVC)
 
     target_include_directories(${DllName} PRIVATE ${XLE_DIR})
+    target_compile_features(${DllName} PUBLIC cxx_std_17)
+    set_property(TARGET ${DllName} PROPERTY CXX_EXTENSIONS OFF)
 
-    if (NOT WIN32)
-        target_link_libraries(${DllName} pthread)
-    else ()
-        target_link_libraries(${DllName} wsock32.lib ws2_32.lib)
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        target_compile_definitions(${DllName} PUBLIC -DXL_DEBUG -D_DEBUG)
+    elseif ( (CMAKE_BUILD_TYPE STREQUAL "Release") OR (CMAKE_BUILD_TYPE STREQUAL "MinSizeRel") OR (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo"))
+        target_compile_definitions(${DllName} PUBLIC -DXL_RELEASE -DNDEBUG)
     endif ()
 endmacro()
 
--- Solutions/CMake/toolchain.cmake
+++ Solutions/CMake/toolchain.cmake
@@ -1,50 +1,56 @@
 
-get_filename_component(MAIN_CMAKE_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
-get_filename_component(XLE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
-get_filename_component(FOREIGN_DIR "${XLE_DIR}/Foreign/" ABSOLUTE)
+set(XLE_CUSTOM_CMAKE_CFG NO CACHE BOOL "Draw compiler settings from the .props files rather than cmake configuration settings")
 
-####################################################################################################
-set(CLANG_BASE_FLAGS "")
-set(WARNING_FLAGS "")
-set(CPP_BASE_FLAGS "")
-set(OPTIMIZER_FLAGS "")
+# We used to disable as much of the cmake logic for compiler and linker flags as possible, and instead
+# just include in .props files (used by the Microsoft build system) for configuring the compiler flags
+# This only works when using the Microsoft toolchain, however, since those props files are specific to
+# that build system.
+if (XLE_CUSTOM_CMAKE_CFG)
 
-####################################################################################################
-# Unset all of the default projection configuration settings provided by cmake
-# Note that we have to explicitly set the exception handling settings for visual studio to get around
-# a hack in the cmake generator for visual studio that always sets the exception handling key to
-# something; even if we've got a better setting in one of our inherit properties files
-if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    set(CMAKE_C_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
-    set(CMAKE_CXX_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
-    set(LINK_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
-    set(CMAKE_STATIC_LINKER_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
-    set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
+    ####################################################################################################
+    set(CLANG_BASE_FLAGS "")
+    set(WARNING_FLAGS "")
+    set(CPP_BASE_FLAGS "")
+    set(OPTIMIZER_FLAGS "")
 
-    set(CMAKE_C_FLAGS "/EHsc" CACHE STRING "work around MSVC generator issue" FORCE)
-    set(CMAKE_CXX_FLAGS "/EHsc" CACHE STRING "work around MSVC generator issue" FORCE)
-endif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    ####################################################################################################
+    # Unset all of the default projection configuration settings provided by cmake
+    # Note that we have to explicitly set the exception handling settings for visual studio to get around
+    # a hack in the cmake generator for visual studio that always sets the exception handling key to
+    # something; even if we've got a better setting in one of our inherit properties files
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        set(CMAKE_C_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
+        set(CMAKE_CXX_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
+        set(LINK_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
+        set(CMAKE_STATIC_LINKER_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
+        set(CMAKE_EXE_LINKER_FLAGS "" CACHE STRING "Clear CMake Settings" FORCE)
 
-####################################################################################################
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    # When using clang, we must use the --whole-archive flag, to ensure that symbols that
-    # are required by sibling projects (ie, if the Math library uses symbols from the Utility library)
-    # can be found.
-    # We also set --gc-sections, (and -ffunction-sections and -fdata-sections on the compiler)
-    # to try to strip out as much as possible after linking
-    set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections,--whole-archive,-z,muldefs" CACHE STRING "Linker flags for clang" FORCE)
+        set(CMAKE_C_FLAGS "/EHsc" CACHE STRING "work around MSVC generator issue" FORCE)
+        set(CMAKE_CXX_FLAGS "/EHsc" CACHE STRING "work around MSVC generator issue" FORCE)
+    endif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 
-    APPEND(CLANG_BASE_FLAGS " $<$<COMPILE_LANGUAGE:CXX>:-std=c++14> $<$<COMPILE_LANGUAGE:C>:-std=c99> -fapple-pragma-pack -fexceptions -frtti -ffunction-sections -fdata-sections  $<$<COMPILE_LANGUAGE:CXX>:-fcxx-exceptions>")
-    if ((BUILD_TYPE_LOWER STREQUAL "release") OR (BUILD_TYPE_LOWER STREQUAL "staging"))
-        set(OPTIMIZER_FLAGS "-Os -finline-functions")
-        add_definitions("-DNDEBUG")
-    else()
-        set(OPTIMIZER_FLAGS "-O0 -ggdb -fno-inline")
-        add_definitions("-DDEBUG=1" "-D_DEBUG")
-    endif()
+    ####################################################################################################
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        # When using clang, we must use the --whole-archive flag, to ensure that symbols that
+        # are required by sibling projects (ie, if the Math library uses symbols from the Utility library)
+        # can be found.
+        # We also set --gc-sections, (and -ffunction-sections and -fdata-sections on the compiler)
+        # to try to strip out as much as possible after linking
+        set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections,--whole-archive,-z,muldefs" CACHE STRING "Linker flags for clang" FORCE)
 
-    APPEND(WARNING_FLAGS " -Wall -Wno-unknown-pragmas -Wno-unused-local-typedef -Wno-unused-const-variable -Wno-reorder -Wno-unused-variable -Wno-unused-function -Wno-char-subscripts")
-    append(WARNING_FLAGS " -Wno-unknown-pragmas -Werror=implicit-function-declaration -Wno-unused-local-typedef")
+        APPEND(CLANG_BASE_FLAGS " $<$<COMPILE_LANGUAGE:CXX>:-std=c++14> $<$<COMPILE_LANGUAGE:C>:-std=c99> -fapple-pragma-pack -fexceptions -frtti -ffunction-sections -fdata-sections  $<$<COMPILE_LANGUAGE:CXX>:-fcxx-exceptions>")
+        if ((BUILD_TYPE_LOWER STREQUAL "release") OR (BUILD_TYPE_LOWER STREQUAL "staging"))
+            set(OPTIMIZER_FLAGS "-Os -finline-functions")
+            add_definitions("-DNDEBUG")
+        else()
+            set(OPTIMIZER_FLAGS "-O0 -ggdb -fno-inline")
+            add_definitions("-DDEBUG=1" "-D_DEBUG")
+        endif()
+
+        APPEND(WARNING_FLAGS " -Wall -Wno-unknown-pragmas -Wno-unused-local-typedef -Wno-unused-const-variable -Wno-reorder -Wno-unused-variable -Wno-unused-function -Wno-char-subscripts")
+        append(WARNING_FLAGS " -Wno-unknown-pragmas -Werror=implicit-function-declaration -Wno-unused-local-typedef")
+    endif ()
+
+    set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "" FORCE)
+
 endif ()
-
-set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "" FORCE)
