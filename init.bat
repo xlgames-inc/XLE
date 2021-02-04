b0a1b006332d080d3e829d01b62ac6410820737c
David Jewsbury <djewsbury@gmail.com>
2021-02-04 11:56:24 +1000

    Template launch.json configuration for vscode

--- init.bat
+++
@@ -1,9 +0,0 @@
-rmdir /S /Q CMakeBuild
-mkdir CMakeBuild
-cd CMakeBuild
-"C:\Program Files\CMake\bin\cmake.exe" ^
-    -DCMAKE_TOOLCHAIN_FILE=../Solutions/CMake/toolchain.cmake ^
-    %* ^
-    -G "Visual Studio 15 2017 Win64" ^
-    ../Solutions/Global/
-cd ..
