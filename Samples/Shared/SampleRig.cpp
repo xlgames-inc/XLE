7863515729fe4b1b892480d68863b05e786a5a85
David Jewsbury <djewsbury@gmail.com>
2021-04-10 09:20:28 +1000

    First step sample rig

--- Samples/Shared/SampleRig.cpp
+++ Samples/Shared/SampleRig.cpp
@@ -23,6 +23,7 @@
 #include "../../RenderCore/Techniques/RenderPassUtils.h"
 #include "../../RenderCore/Techniques/ParsingContext.h"
 #include "../../RenderCore/Techniques/Services.h"
+#include "../../RenderCore/Techniques/ImmediateDrawables.h"
 
 #include "../../RenderOverlays/Font.h"
 #include "../../PlatformRig/DebugHotKeys.h"
@@ -30,6 +31,11 @@
 
 #include "../../Assets/AssetServices.h"
 #include "../../Assets/AssetSetManager.h"
+#include "../../Assets/IFileSystem.h"
+#include "../../Assets/MountingTree.h"
+#include "../../Assets/OSFileSystem.h"
+#include "../../Assets/IntermediateCompilers.h"
+#include "../../Assets/CompileAndAsyncManager.h"
 
 #include "../../ConsoleRig/Console.h"
 #include "../../OSServices/Log.h"
@@ -44,12 +50,26 @@
 
 #include "../../RenderCore/Metal/DeviceContext.h"		// (for PrepareForDestruction)
 
+#include "../../RenderCore/Assets/PredefinedPipelineLayout.h"
+#include "../../RenderCore/Assets/PipelineConfigurationUtils.h"
+#include "../../RenderCore/Techniques/CompiledShaderPatchCollection.h"
+#include "../../RenderCore/Vulkan/IDeviceVulkan.h"
+#include "../../RenderOverlays/FontRendering.h"
+#include "../../Assets/Assets.h"
+
 namespace Sample
 {
 	static void InitProfilerDisplays(
 		RenderOverlays::DebuggingDisplay::DebugScreensSystem& debugSys, 
 		RenderCore::IAnnotator* annotator,
 		Utility::HierarchicalCPUProfiler& cpuProfiler);
+
+    static std::shared_ptr<RenderCore::Techniques::IImmediateDrawables> CreateImmediateDrawables(
+        const std::shared_ptr<RenderCore::IDevice>&,
+        RenderCore::Assets::PredefinedPipelineLayoutFile& pipelineLayout,
+        const std::string& pipelineLayoutName);
+
+    static std::shared_ptr<RenderCore::ILowLevelCompiler> CreateDefaultShaderCompiler(RenderCore::IDevice& device);
 
 	void ExecuteSample(std::shared_ptr<ISampleOverlay>&& sampleOverlay)
     {
@@ -73,14 +93,28 @@ namespace Sample
 				RenderCore::PresentationChainDesc{unsigned(clientRect.second[0] - clientRect.first[0]), unsigned(clientRect.second[1] - clientRect.first[1])});
 		RenderCore::Techniques::SetThreadContext(sampleGlobals._renderDevice->GetImmediateContext());
 
-        auto assetServices = ConsoleRig::MakeAttachablePtr<::Assets::Services>(0);
+        auto assetServices = ConsoleRig::MakeAttachablePtr<::Assets::Services>();
         auto renderAssetServices = ConsoleRig::MakeAttachablePtr<RenderCore::Assets::Services>(sampleGlobals._renderDevice);
 		auto techniquesServices = ConsoleRig::MakeAttachablePtr<RenderCore::Techniques::Services>(sampleGlobals._renderDevice);
 
+        auto rawosmnt = ::Assets::MainFileSystem::GetMountingTree()->Mount("rawos", ::Assets::CreateFileSystem_OS());
+
 		::ConsoleRig::GlobalServices::GetInstance().LoadDefaultPlugins();
 
+        auto shaderCompiler = CreateDefaultShaderCompiler(*sampleGlobals._renderDevice);
+		auto shaderService = std::make_unique<RenderCore::ShaderService>();
+		auto shaderSource = std::make_shared<RenderCore::MinimalShaderSource>(shaderCompiler);
+		shaderService->SetShaderSource(shaderSource);
+        
+        auto& compilers = ::Assets::Services::GetAsyncMan().GetIntermediateCompilers();
+		auto filteringRegistration = ShaderSourceParser::RegisterShaderSelectorFilteringCompiler(compilers);
+		auto shaderCompilerRegistration = RenderCore::RegisterShaderCompiler(shaderSource, compilers);
+		auto shaderCompiler2Registration = RenderCore::Techniques::RegisterInstantiateShaderGraphCompiler(shaderSource, compilers);
+
+        // Verbose.SetConfiguration({});
+
         {
-			auto attachmentPool = std::make_shared<RenderCore::Techniques::AttachmentPool>();
+			auto attachmentPool = std::make_shared<RenderCore::Techniques::AttachmentPool>(sampleGlobals._renderDevice);
 			auto frameBufferPool = std::make_shared<RenderCore::Techniques::FrameBufferPool>();
 
 				//  Tie in the window handler so we get presentation chain resizes, and give our
@@ -96,8 +130,8 @@ namespace Sample
 			sampleGlobals._techniqueContext = std::make_shared<PlatformRig::GlobalTechniqueContext>();
 
                 // currently we need to maintain a reference on these two fonts -- 
-            auto defaultFont0 = RenderOverlays::GetX2Font("Raleway", 16);
-            auto defaultFont1 = RenderOverlays::GetX2Font("Vera", 16);
+            // auto defaultFont0 = RenderOverlays::GetX2Font("Raleway", 16);
+            // auto defaultFont1 = RenderOverlays::GetX2Font("Vera", 16);
 			Utility::HierarchicalCPUProfiler cpuProfiler;
 
                 //  Create the debugging system, and add any "displays"
@@ -105,14 +139,18 @@ namespace Sample
                 //  useful to create a debugging display to go along with any new feature. 
                 //  It just provides a convenient architecture for visualizing important information.
             Log(Verbose) << "Setup tools and debugging" << std::endl;
-            PlatformRig::FrameRig frameRig;
+            auto pipelineLayout = ::Assets::MakeAsset<RenderCore::Assets::PredefinedPipelineLayoutFile>("xleres/TechniqueLibrary/Config/Immediate.pipeline");
+            pipelineLayout->StallWhilePending();
+            auto immediateDrawables = CreateImmediateDrawables(sampleGlobals._renderDevice, *pipelineLayout->Actualize(), "ImmediateDrawables");
+            auto fontRenderer = std::make_shared<RenderOverlays::FontRenderingManager>(*sampleGlobals._renderDevice);
+            PlatformRig::FrameRig frameRig(immediateDrawables, fontRenderer);
 			auto threadContext = sampleGlobals._renderDevice->GetImmediateContext();
 
-            PlatformRig::InitDebugDisplays(*frameRig.GetDebugSystem());
-            InitProfilerDisplays(*frameRig.GetDebugSystem(), &threadContext->GetAnnotator(), cpuProfiler);
+            // PlatformRig::InitDebugDisplays(*frameRig.GetDebugSystem());
+            // InitProfilerDisplays(*frameRig.GetDebugSystem(), &threadContext->GetAnnotator(), cpuProfiler);
 
             auto overlaySwitch = std::make_shared<PlatformRig::OverlaySystemSwitch>();
-            overlaySwitch->AddSystem(PlatformRig::KeyId_Make("~"), PlatformRig::CreateConsoleOverlaySystem());
+            overlaySwitch->AddSystem(PlatformRig::KeyId_Make("~"), PlatformRig::CreateConsoleOverlaySystem(immediateDrawables, fontRenderer));
             frameRig.GetDebugScreensOverlaySystem()->AddSystem(overlaySwitch);
 
 			sampleGlobals._debugScreens = frameRig.GetDebugSystem();
@@ -129,6 +167,9 @@ namespace Sample
             window.GetInputTranslator().AddListener(sampleGlobals._mainInputHander);
 
             Log(Verbose) << "Setup frame rig and rendering context" << std::endl;
+
+            char currentDir[256];
+            OSServices::GetCurrentDirectory(dimof(currentDir), currentDir);
             
 			frameRig.GetMainOverlaySystem()->AddSystem(sampleOverlay);
 			sampleOverlay->OnStartup(sampleGlobals);
@@ -142,7 +183,7 @@ namespace Sample
 					parserContext, &cpuProfiler);
 
                     // ------- Update ----------------------------------------
-                RenderCore::Techniques::Services::GetBufferUploads().Update(*threadContext, false);
+                RenderCore::Techniques::Services::GetBufferUploads().Update(*threadContext);
 				sampleOverlay->OnUpdate(frameResult._elapsedTime * Tweakable("TimeScale", 1.0f));
                 cpuProfiler.EndFrame();
             }
@@ -159,20 +200,24 @@ namespace Sample
         Log(Verbose) << "Starting shutdown" << std::endl;
         // assetServices->GetAssetSets().LogReport();
         RenderCore::Metal::DeviceContext::PrepareForDestruction(sampleGlobals._renderDevice.get(), sampleGlobals._presentationChain.get());
-
         assetServices->GetAssetSets().Clear();
+
+        compilers.DeregisterCompiler(shaderCompiler2Registration._registrationId);
+		compilers.DeregisterCompiler(shaderCompilerRegistration._registrationId);
+		compilers.DeregisterCompiler(filteringRegistration._registrationId);
+        ::Assets::MainFileSystem::GetMountingTree()->Unmount(rawosmnt);
+
 		ConsoleRig::ResourceBoxes_Shutdown();
-		TerminateFileSystemMonitoring();
 		::ConsoleRig::GlobalServices::GetInstance().UnloadDefaultPlugins();
 		assetServices->GetAssetSets().Clear();
 		ConsoleRig::ResourceBoxes_Shutdown();
 
-		techniquesServices.reset();
-		renderAssetServices.reset();
-        assetServices.reset();
+		techniquesServices = nullptr;
+		renderAssetServices = nullptr;
+        assetServices = nullptr;
     }
 
-	static void InitProfilerDisplays(
+	void InitProfilerDisplays(
 		RenderOverlays::DebuggingDisplay::DebugScreensSystem& debugSys, 
 		RenderCore::IAnnotator* annotator,
 		Utility::HierarchicalCPUProfiler& cpuProfiler)
@@ -185,6 +230,38 @@ namespace Sample
             std::make_shared<PlatformRig::Overlays::HierarchicalProfilerDisplay>(&cpuProfiler),
             "[Profiler] CPU Profiler");
     }
+
+    std::shared_ptr<RenderCore::Techniques::IImmediateDrawables> CreateImmediateDrawables(
+        const std::shared_ptr<RenderCore::IDevice>& device,
+        RenderCore::Assets::PredefinedPipelineLayoutFile& pipelineLayoutFile,
+        const std::string& pipelineLayoutName)
+    {
+        auto i = pipelineLayoutFile._pipelineLayouts.find(pipelineLayoutName);
+        if (i == pipelineLayoutFile._pipelineLayouts.end())
+            Throw(std::runtime_error("Did not find pipeline layout with the name " + pipelineLayoutName + " in the given pipeline layout file"));
+        auto pipelineInit = i->second->MakePipelineLayoutInitializer(RenderCore::ShaderLanguage::HLSL);
+        auto pipelineLayout = device->CreatePipelineLayout(pipelineInit);
+        return RenderCore::Techniques::CreateImmediateDrawables(
+            device, 
+            pipelineLayout,
+            RenderCore::Techniques::FindLayout(pipelineLayoutFile, pipelineLayoutName, "Material"),
+            RenderCore::Techniques::FindLayout(pipelineLayoutFile, pipelineLayoutName, "Sequencer"));
+    }
+
+    std::shared_ptr<RenderCore::ILowLevelCompiler> CreateDefaultShaderCompiler(RenderCore::IDevice& device)
+	{
+		auto* vulkanDevice  = (RenderCore::IDeviceVulkan*)device.QueryInterface(typeid(RenderCore::IDeviceVulkan).hash_code());
+		if (vulkanDevice) {
+			// Vulkan allows for multiple ways for compiling shaders. The tests currently use a HLSL to GLSL to SPIRV 
+			// cross compilation approach
+			RenderCore::VulkanCompilerConfiguration cfg;
+			cfg._shaderMode = RenderCore::VulkanShaderMode::HLSLCrossCompiled;
+			cfg._legacyBindings = RenderCore::Assets::CreateDefaultLegacyRegisterBindingDesc();
+		 	return vulkanDevice->CreateShaderCompiler(cfg);
+		} else {
+			return device.CreateShaderCompiler();
+		}
+	}
 
 	void ISampleOverlay::OnStartup(const SampleGlobals& globals) {}
 	void ISampleOverlay::OnUpdate(float deltaTime) {}
